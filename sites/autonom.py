f12d7d8ae13f0b176c2c5c6c324aaef7b9e9d31f
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-14 01:59:16 +0300

    refactoring autonom

--- sites/autonom.py
+++ sites/autonom.py
@@ -1,39 +1,9 @@
-from scraper_peviitor import Scraper, Rules, loadingData, ScraperSelenium
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-
-import time
+from scraper_peviitor import Scraper, Rules, loadingData
 import uuid 
-import os
 
-#Folosim ScraperSelenium deoarece paginatia se face cu ajutorul unui buton
-scraper = ScraperSelenium("https://www.autonom.ro/cariere")
-scraper.get()
+url = "https://www.autonom.ro/cariere"
 
-#Asteptam sa se incarce cookie-ul
-scraper.wait(EC.presence_of_element_located((By.CLASS_NAME, 'autonom_gdpr_cookie_bar_accept')))
-
-#Acceptam cookie-ul
-cookieBtn = scraper.find_element(By.CLASS_NAME, 'autonom_gdpr_cookie_bar_accept')
-scraper.click(cookieBtn)
-
-#Asteptam sa se incarce butonul de load more
-moreJobsBtn = scraper.find_element(By.ID, 'loadMore')
-scraper.driver.execute_script("arguments[0].scrollIntoView();", moreJobsBtn)
-
-#Daca butonul de load more este afisat, atunci il apasam
-while moreJobsBtn.is_displayed():
-    scraper.click(moreJobsBtn)
-    time.sleep(2)
-else:
-    print("No more jobs")
-
-#Obtinem DOM-ul
-dom = scraper.getDom()
-
-#Incarcam DOM-ul in BeautifulSoup
-scraper = Scraper()
-scraper.soup = dom
+scraper = Scraper(url)
 rules = Rules(scraper)
 
 #Obtinem toate joburile
