9b992a4897bed4eeef6fd5d30ca6359c4410f64b
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-14 11:11:48 +0300

    refactoring fedex.py

--- sites/fedex.py
+++ sites/fedex.py
@@ -1,37 +1,25 @@
-from scraper_peviitor import Scraper, Rules, loadingData, ScraperSelenium
-from selenium.webdriver import Chrome
+from scraper_peviitor import Scraper, loadingData
 
-import time
 import uuid
-import os
 
 #Folosim ScraperSelenium deoaarece joburile sunt incarcate prin AJAX
-scraper = ScraperSelenium("https://careers.fedex.com/express-eu/jobs?lang=ro-RO&location=România&woe=12&stretch=10&stretchUnit=MILES&page=1&limit=100")
-scraper.get()
+scraper = Scraper('https://careers.fedex.com/api/jobs?lang=ro-RO&location=Rom%25C3%25A2nia&woe=12&stretch=10&stretchUnit=MILES&page=1&limit=100&sortBy=relevance&descending=false&internal=false&brand=FedEx%20Express%20EU')
 
-time.sleep(5)
+jobs = scraper.getJson().get('jobs')
 
-#Folosim Scraper pentru a extrage codul HTML
-dom = scraper.getDom()
-
-#Setam un nou scraper pentru a extrage joburile
-scraper = Scraper()
-scraper.soup = dom
-rules = Rules(scraper)
-
-#Extragem joburile
-jobs = rules.getTags("a", {"class": "job-title-link"})
+print(len(jobs))
 
 finaljobs = list()
 
 #Iteram prin joburi si le adaugam in lista finaljobs
 for job in jobs:
+    obj = job.get('data')
     id = uuid.uuid4()
-    job_title = job.text
-    job_link = "https://careers.fedex.com" + job.get("href")
+    job_title = obj.get('title')
+    job_link = obj.get('meta_data').get('canonical_url')
     company = "FedEx"
     country = "Romania"
-    city = "Romania"
+    city = obj.get("city")
 
     print(job_title + " -> " + city)
 
