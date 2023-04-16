4c0ef8e84f8393fe43bf25d3b5be710f9b4424f7
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-17 00:27:59 +0300

    create scraper for libra bank

---
+++ sites/libra_bank.py
@@ -0,0 +1,35 @@
+from scraper_peviitor import Scraper, Rules, loadingData
+import uuid
+
+url = "https://www.librabank.ro/Cariere"
+
+scraper = Scraper(url)
+rules = Rules(scraper)
+
+jobContainer = rules.getTags("div", {"class": "jobListing"})
+jobs = list(jobContainer)[0].find_all("div", {"class": "card-body"})
+
+finalJobs = list()
+
+for job in jobs:
+    id = uuid.uuid4()
+    job_title = job.find("a").text.strip()
+    job_link = "https://www.librabank.ro" + job.find("a").get("href")
+    company = "Libra Bank"
+    country = "Romania"
+    city = "Romania"
+
+    finalJobs.append({
+        "id": str(id),
+        "job_title": job_title,
+        "job_link": job_link,
+        "company": company,
+        "country": country,
+        "city": city
+    })
+
+    print(job_title + " -> " + city)
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", "Libra Bank")
