301d9c54c0ae8b49956a43465e2df6b3b4880c23
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-16 19:05:09 +0300

    create scraper for tbi bank

---
+++ sites/tbi_bank.py
@@ -0,0 +1,34 @@
+from scraper_peviitor import Scraper, Rules, loadingData
+import uuid
+
+url = "https://tbibank.ro/cariere/"
+
+scraper = Scraper(url)
+rules = Rules(scraper)
+
+jobs = rules.getTags("div", {"class": "card-career"})
+
+finalJobs = list()
+
+for job in jobs:
+    id = uuid.uuid4()
+    job_title = job.find("div", {"class": "card-career__header"}).text.strip()
+    job_link = job.find("a", {"class": "card-career__link"}).get("href")
+    company = "TBI Bank"
+    country = "Romania"
+    city = job.find("div", {"class": "card-career__countries"}).text.strip()
+
+    print(job_title + " -> " + city)
+
+    finalJobs.append({
+        "id": str(id),
+        "job_title": job_title,
+        "job_link": job_link,
+        "company": company,
+        "country": country,
+        "city": city
+    })
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", "TBI Bank")
