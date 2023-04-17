012c612824ebd876830ced88e66f96f9af0e58be
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-18 00:41:53 +0300

    create scraper for keineken

---
+++ sites/heineken.py
@@ -0,0 +1,42 @@
+from scraper_peviitor import Scraper, Rules, loadingData
+import uuid
+
+scraper = Scraper()
+rules = Rules(scraper)
+
+jobsFound = True
+
+startRow = 0
+
+finalJobs = list()
+
+while jobsFound:
+    scraper.url = f"https://careers.theheinekencompany.com/search/?createNewAlert=false&q=&locationsearch=Romania&startrow={startRow}"
+    jobs= rules.getTags("tr", {"class": "data-row"})
+    for job in jobs:
+        if job.find("span", {"class": "jobLocation"}).text.split(",")[1].strip() != "RO":
+            jobsFound = False
+            break
+        id = uuid.uuid4()
+        job_title = job.find("span", {"class": "jobTitle"}).text.strip()
+        job_link = "https://careers.theheinekencompany.com" + job.find("a", {"class": "jobTitle-link"}).get("href")
+        company = "Heineken"
+        country = "Romania"
+        city = job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip()
+
+        print(job_title + " -> " + city)
+
+        finalJobs.append({
+            "id": str(id),
+            "job_title": job_title,
+            "job_link": job_link,
+            "company": company,
+            "country": country,
+            "city": city
+        })
+
+    startRow += 25
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", "Heineken")
