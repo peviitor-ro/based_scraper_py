8754687f0c6492386fe71a5970d98ecace22d177
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-04-25 01:14:47 +0300

    update log scripts

--- sites/main.py
+++ sites/main.py
@@ -6,12 +6,11 @@ path = os.path.dirname(os.path.abspath(__file__))
 
 for site in os.listdir(path):
     if site.endswith('.py') and site not in exclude:
-        action = subprocess.run(['python', os.path.join(path, site)])   
+        action = subprocess.run(['python', os.path.join(path, site)], capture_output=True)   
         if action.returncode != 0:
             errors = action.stderr.decode('utf-8')
             print("Error in " + site)
             print(errors)
-        else:
-            print("Success in " + site)
+
 
         
