cc758e864979dd69480614bc2edd1e37cb73cd36
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-02 18:10:25 +0300

    rolback

--- sites/auchan.py
+++ sites/auchan.py
@@ -3,63 +3,43 @@ from scraper_peviitor import Scraper, Rules, loadingData
 import uuid
 import re
 import json
-from selenium import webdriver
-import platform
 
-
-# Folosim ScraperSelenium pentru a putea naviga pe pagini
+#Folosim ScraperSelenium pentru a putea naviga pe pagini
 url = "https://cariere.auchan.ro"
+scraper = Scraper()
+scraper.session.verify = False
+scraper.url = url
+rules = Rules(scraper)
 
-if platform.system() == 'Darwin':
-    print("running on macOS")
-    driver = webdriver.Chrome()
-elif platform.system() == 'Linux':
-    print("running on linux")
-    options = webdriver.FirefoxOptions()
-    options.add_argument('--headless')
-    driver = webdriver.Firefox(options=options)
+doomBody = rules.getTag("body")
+regex = re.compile(r'vmCfg = {(.*)}')
 
-driver.get(url)
+jobs = re.search(regex, doomBody.prettify()).group(1)
 
-html = driver.page_source
+jobs = json.loads("{" + jobs + "}")
 
-print(html)
+finaljobs = list()
 
-# print(process.stdout.decode('utf-8'))
-# scraper = Scraper()
-# scraper.session.verify = False
-# scraper.url = url
-# rules = Rules(scraper)
-
-# doomBody = rules.getTag("body")
-# regex = re.compile(r'vmCfg = {(.*)}')
-
-# jobs = re.search(regex, doomBody.prettify()).group(1)
-
-# jobs = json.loads("{" + jobs + "}")
-
-# finaljobs = list()
-
-# for job in jobs.get("PositionList"):
-#     id = uuid.uuid4()
-#     job_title = job.get("PositionName")
-#     job_link = "https://cariere.auchan.ro/" + f"Position/Details?id={job.get('PositionId')}"
-#     company = "Auchan"
-#     country = "Romania"
-#     city = job.get("CityList")
+for job in jobs.get("PositionList"):
+    id = uuid.uuid4()
+    job_title = job.get("PositionName")
+    job_link = "https://cariere.auchan.ro/" + f"Position/Details?id={job.get('PositionId')}"
+    company = "Auchan"
+    country = "Romania"
+    city = job.get("CityList")
     
-#     print(job_title + " -> " + city)
-#     finaljobs.append({
-#         "id": str(id),
-#         "job_title": job_title,
-#         "job_link": job_link,
-#         "company": company,
-#         "country": country,
-#         "city": city
-#     })
+    print(job_title + " -> " + city)
+    finaljobs.append({
+        "id": str(id),
+        "job_title": job_title,
+        "job_link": job_link,
+        "company": company,
+        "country": country,
+        "city": city
+    })
 
-# #Afisam numarul total de joburi
-# print("Total jobs: " + str(len(finaljobs)))
+#Afisam numarul total de joburi
+print("Total jobs: " + str(len(finaljobs)))
 
-# #Incarcam datele in baza de date
-# loadingData(finaljobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", "Auchan")
+#Incarcam datele in baza de date
+loadingData(finaljobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", "Auchan")
--- sites/main.py
+++ sites/main.py
@@ -1,21 +1,19 @@
 import os 
 import subprocess
 
-exclude = ['__init__.py','main.py' , 'kaufland.py', "tesla.py"]
+exclude = ['__init__.py','main.py' , 'kaufland.py', "tesla.py", "auchan.py"]
 path = os.path.dirname(os.path.abspath(__file__))
 
 for site in os.listdir(path):
     if site.endswith('.py') and site not in exclude:
-        # action = subprocess.run(['python', os.path.join(path, site)], capture_output=True)   
-        action = subprocess.run(['python', os.path.join(path, "auchan.py")])
+        action = subprocess.run(['python', os.path.join(path, site)], capture_output=True)   
         if action.returncode != 0:
             errors = action.stderr.decode('utf-8')
             print("Error in " + site)
             print(errors)
         else:
             print("Success scraping " + site)
-            print(action.stdout.decode('utf-8'))
-    break
+
 
 
         
