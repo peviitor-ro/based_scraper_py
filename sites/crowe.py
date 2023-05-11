4fc8e448021267d1bdbf92f45dafdbc802a9c748
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-11 17:33:03 +0300

    create scraper for crowe

---
+++ sites/crowe.py
@@ -0,0 +1,55 @@
+from scraper_peviitor import Scraper, Rules, loadingData
+import uuid
+import json
+
+page = 0
+scraper = Scraper()
+rules = Rules(scraper)
+
+company = {"company": "Crowe"}
+finalJobs = list()
+
+while True:
+    url = f"https://www.crowe.com/api/sitecore/ListWithFilter/GetTable?sc_mode=normal&id=%7B7D8ADF0E-32CD-4955-8B4C-C4F8561881AE%7D&languageName=ro-RO&page={page}&isDateDesc=true&enforceNormal=True"
+    scraper.url = url
+
+    jobs = rules.getTags("div", {"class": "news-list-table__table__item__link"})
+
+    if len(jobs) != 0:
+        for job in jobs:
+            id = uuid.uuid4()
+            job_title = job.find("a").text.strip()
+            job_link = job.find("a").get("href")
+            country = "Romania"
+            city = "Romania"
+
+            print(job_title + " -> " + city)
+            
+            finalJobs.append({
+                "id": str(id),
+                "job_title": job_title,
+                "job_link": job_link,
+                "country": country,
+                "city": city,
+                "company": company.get("company")
+            })
+    else:
+        break
+
+    page += 1
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", company.get("company"))
+
+logoUrl = "https://i.ytimg.com/vi/dTmm3WNIpnc/maxresdefault.jpg"
+
+scraper.session.headers.update({
+    "Content-Type": "application/json",
+})
+scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
+    {
+        "id":company.get("company"),
+        "logo":logoUrl
+    }
+]))
