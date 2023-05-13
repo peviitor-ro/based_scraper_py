8d2f362a886d95617e21b89bee1d28719b510d76
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-13 14:02:05 +0300

    create scraper for thoughtworks

---
+++ sites/thoughtworks.py
@@ -0,0 +1,48 @@
+from scraper_peviitor import Scraper, loadingData
+import uuid
+import json
+
+url = "https://www.thoughtworks.com/rest/careers/jobs"
+
+company = {"company": "ThoughtWorks"}
+finalJobs = list()
+
+scraper = Scraper(url)
+
+jobs = scraper.getJson().get("jobs")
+
+for job in jobs:
+    country = job.get("country")
+
+    if country == "Romania":
+        id = uuid.uuid4()
+        job_title = job.get("name")
+        job_link = "https://www.thoughtworks.com/careers/jobs/" + str(job.get("sourceSystemId"))
+        city = job.get("city")
+
+        print(job_title + " -> " + city)
+
+        finalJobs.append({
+            "id": str(id),
+            "job_title": job_title,
+            "job_link": job_link,
+            "country": country,
+            "city": city,
+            "company": company.get("company")
+        })
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", company.get("company"))
+
+logoUrl = "https://www.thoughtworks.com/etc.clientlibs/thoughtworks/clientlibs/clientlib-site/resources/images/thoughtworks-logo.svg"
+
+scraper.session.headers.update({
+    "Content-Type": "application/json",
+})
+scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
+    {
+        "id":company.get("company"),
+        "logo":logoUrl
+    }
+]))
