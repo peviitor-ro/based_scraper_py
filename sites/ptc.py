9adacf8e5768495ec97fc8a5a470abfb871fbd71
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-13 13:50:21 +0300

    create sscraper for ptc

---
+++ sites/ptc.py
@@ -0,0 +1,51 @@
+from scraper_peviitor import Scraper, loadingData
+import uuid
+import json
+
+url = "https://ptc.eightfold.ai/api/apply/v2/jobs?domain=ptc.com&start=0&location=Bucharest%2C%20Romania&domain=ptc.com&sort_by=relevance"
+
+company = {"company": "PTC"}
+finalJobs = list()
+
+scraper = Scraper(url)
+
+totalJobs = scraper.getJson().get("count")
+
+pages = [*range(0, totalJobs, 10)]
+
+for page in pages:
+    scraper.url = url.replace("start=0", "start=" + str(page))
+    jobs = scraper.getJson().get("positions")
+    for job in jobs:
+        id = uuid.uuid4()
+        job_title = job.get("name")
+        job_link = "https://ptc.eightfold.ai/careers?pid=" + str(job.get("id")) + "&domain=ptc.com&sort_by=relevance"
+        country = "Romania"
+        city = job.get("location").split(",")[0]
+
+        print(job_title + " -> " + city)
+        
+        finalJobs.append({
+            "id": str(id),
+            "job_title": job_title,
+            "job_link": job_link,
+            "country": country,
+            "city": city,
+            "company": company.get("company")
+        })
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", company.get("company"))
+
+logoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1d/PTC_logo.svg/1280px-PTC_logo.svg.png"
+
+scraper.session.headers.update({
+    "Content-Type": "application/json",
+})
+scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
+    {
+        "id":company.get("company"),
+        "logo":logoUrl
+    }
+]))
