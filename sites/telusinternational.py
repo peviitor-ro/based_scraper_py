d87fb59dd0c44dc54f7c73f35a6155fd85be5851
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-05-20 13:15:45 +0300

    create scraper for telusinterational

---
+++ sites/telusinternational.py
@@ -0,0 +1,46 @@
+from scraper_peviitor import Scraper, Rules, loadingData
+import uuid
+import json
+
+url = "https://jobs.telusinternational.com/en_US/careers/Romania?source=TI+website&amp%3Btags=telus_main_website&listFilterMode=1&2947=5170&2947_format=4626"
+
+company = {"company": "TelusInternational"}
+finalJobs = list()
+
+scraper = Scraper(url)
+rules = Rules(scraper)
+
+jobs = rules.getTags("li", {"class": "listSingleColumnItem"})
+
+for job in jobs:
+    id = uuid.uuid4()
+    job_title = job.find("h3").text.strip()
+    job_link = job.find("h3").find("a").get("href")
+    city = "Romania"
+
+    print(job_title + " -> " + city)
+
+    finalJobs.append({
+        "id": str(id),
+        "job_title": job_title,
+        "job_link": job_link,
+        "company": company.get("company"),
+        "country": "Romania",
+        "city": city
+    })
+
+print("Total jobs: " + str(len(finalJobs)))
+
+loadingData(finalJobs, "182b157-bb68-e3c5-5146-5f27dcd7a4c8", company.get("company"))
+
+logoUrl = "https://jobs.telusinternational.com/portal/11/images/logo_telus-international_header-v2.svg"
+
+scraper.session.headers.update({
+    "Content-Type": "application/json",
+})
+scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
+    {
+        "id":company.get("company"),
+        "logo":logoUrl
+    }
+]))
