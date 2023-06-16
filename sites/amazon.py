67d602de2aa3782206d6815bd8943f86f8386f92
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-06-16 17:37:30 +0300

    repair amazon scraper

--- sites/amazon.py
+++ sites/amazon.py
@@ -2,7 +2,7 @@ from scraper_peviitor import Scraper, loadingData
 import uuid
 import json
 
-url = "https://www.amazon.jobs/en/search.json?normalized_country_code%5B%5D=ROU&radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset=0&result_limit=10&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=&base_query=&city=&country=&region=&county=&query_options=&location%5B%5D=bucharest-romania&"
+url = "https://www.amazon.jobs/en/search.json?normalized_country_code%5B%5D=ROU&radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset=&result_limit=100&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=&base_query=&city=&country=&region=&county=&query_options=&location%5B%5D=bucharest-romania&"
 
 company = {"company": "Amazon"}
 finalJobs = list()
