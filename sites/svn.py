a65afd5a5df79fd722f723daff2fb406aa2cd2c7
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-07-31 11:00:51 +0300

    create scraper for SVN

--- sites/svn.py
+++ sites/svn.py
@@ -13,11 +13,15 @@ jobs_elements = scraper.find('div', class_='jobs').find_all('div', class_='job')
 
 for job in jobs_elements:
     jobs.append(create_job(
-        job_titl=job.find('h3').text,
+        job_title=job.find('h3').text,
         job_link='https://jobs.svn.ro' + job.find('a')['href'],
         city=job.find('ul').find_all('li')[-1].text,
         country="Romania",
         company=company,
     ))
 
+for version in [1,4]:
+    publish(version, company, jobs, 'APIKEY')
+
+publish_logo(company, 'https://www.svn.ro/assets/images/logo/3.png')
 show_jobs(jobs)
