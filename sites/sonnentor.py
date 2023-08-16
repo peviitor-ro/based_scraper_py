27d7938a9b5b57495c1823b9b0f3e36593477439
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-08-16 11:57:42 +0300

    create scraper for sonnentor

---
+++ sites/sonnentor.py
@@ -0,0 +1,27 @@
+from scraper.Scraper import Scraper
+from utils import (publish, publish_logo, create_job, show_jobs)
+
+company = 'SonnentoR'
+jobs = []
+
+url = 'https://www.sonnentor.ro/despre-noi/cariere.html'
+
+scraper = Scraper()
+scraper.get_from_url(url)
+
+jobs_elements = scraper.find('section', class_='js-go-to-link').find_all('div', class_='row')
+
+for job in jobs_elements:
+    jobs.append(create_job(
+        company=company,
+        job_title=job.find('a').text.strip(),
+        job_link=job.find('a')['href'],
+        city='Romania',
+        country='Romania'
+    ))
+
+for version in [1,4]:
+    publish(version, company, jobs, 'APIKEY')
+
+publish_logo(company, 'https://www.sonnentor.ro/pub/static/version1690913752/frontend/sonnentor/ultimate/ro_RO/images/logo.png')
+show_jobs(jobs)
