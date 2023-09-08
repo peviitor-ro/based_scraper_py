9484e82f63c631d938e11f8e984bbc392a66a178
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-09-08 17:14:18 +0300

    fixed Strabag

--- sites/strabag.py
+++ sites/strabag.py
@@ -1,56 +1,44 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
-import json
+from scraper.Scraper import Scraper
+from utils import (publish, publish_logo, create_job, show_jobs)
 
 url = "https://jobboerse.strabag.at/inc/jobsuche_2022.php"
 
-company = {"company": "Strabag"}
-finaljobs = list()
-
-scraper = Scraper()
-rules = Rules(scraper)
+company = "Strabag"
+jobs = list()
 
 data = {
     "MIME Type":"application/x-www-form-urlencoded; charset=UTF-8",
-    # "morejobs":0
+    "morejobs":1,
     "search":"Romania",
     "radius":50,
     "language":"RO",
     "status":1
 }
 
-response = scraper.post(url, data=data)
+while True:
 
-scraper.soup = response.text
+    scraper = Scraper()
+    response = scraper.post(url, data=data).text
+    scraper.__init__(response, "html.parser")
 
-jobs = rules.getTags("div", {"class": "row datenSatz dunkelGrau"})
+    jobs_containers = scraper.find_all("div", class_="row datenSatz dunkelGrau")
 
-for job in jobs:
-    id = uuid.uuid4()
-    job_title = job.find("div", {"class": "row"}).find_all("div")[0].text.strip()
-    job_link = job.find("a").get("href")
-    
-    finaljobs.append({
-        "id": str(id),
-        "job_title": job_title,
-        "job_link": job_link,
-        "company": company.get("company"),
-        "country": "Romania",
-        "city": "Romania"
-    })
+    if len(jobs_containers) == 0:
+        break
 
-print(json.dumps(finaljobs, indent=4))
+    for job_container in jobs_containers:
+        jobs.append(create_job(
+            job_title=job_container.find("div", class_="row").find_all("div")[0].text.strip(),
+            job_link=job_container.find("a")['href'],
+            country="Romania",
+            city="Romania",
+            company=company,
+        ))
 
-loadingData(finaljobs, company.get("company"))
+    data['morejobs'] += 1
 
-logoUrl = "https://jobboerse.strabag.at/img/strabag-logo-300px.png"
+for version in [1,4]:
+    publish(version, company, jobs, 'APIKEY')
 
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
-    {
-        "id":company.get("company"),
-        "logo":logoUrl
-    }
-]))
+publish_logo(company, "https://jobboerse.strabag.at/img/strabag-logo-300px.png")
+show_jobs(jobs)
