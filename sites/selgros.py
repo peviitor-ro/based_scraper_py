c4a5ecb1618eebf2f8130403506c6b7bfe5f6bfb
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-09-25 00:12:19 +0300

    Fixed Selgros

--- sites/selgros.py
+++ sites/selgros.py
@@ -1,46 +1,30 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
-import json
+from scraper.Scraper import Scraper
+from utils import (publish, publish_logo, create_job, show_jobs)
 
-#Cream o instanta a clasei Scraper
-url = "https://www.selgros.ro/posturi-disponibile?p=1"
-scraper = Scraper(url)
-rules = Rules(scraper)
+company = 'Selgros'
+url = 'https://mingle.ro/api/boards/mingle/jobs?q=companyUid~eq~%22selgros%22&page=0&pageSize=1000&sort=modifiedDate~DESC'
 
-#Obtinem numarul total de pagini
-numberPages = int(rules.getTag("a", {"class": "last"}).find_all("span")[1].text)
+scraper = Scraper()
+scraper.get_from_url(url, "JSON")
 
-company = {"company": "Selgros"}
-finaljobs = list()
+jobs = []
 
-#Iteram prin fiecare pagina
-for page in range(1, numberPages + 1):
-    #Setam url-ul paginii curente
-    url = "https://www.selgros.ro/posturi-disponibile?p=" + str(page)
-    scraper.url = url
+for job in scraper.markup['data']['results']:
+    try:
+        city = job['locations'][0]['name']
+    except:
+        city = 'Romania'
 
-    #Luam toate joburile de pe pagina curenta
-    jobs = rules.getTags("div", {"class": "text-content"})
-    
-    #Iteram prin fiecare job
-    for job in jobs:
-        id = uuid.uuid4()
-        job_title = job.find("div", {"class":"title"}).text
-        job_link = job.find("a")["href"]
-        city = job.find("div", {"class":"type-location"}).text.split(",")[1]
+    jobs.append(create_job(
+        job_title=job['jobTitle'],
+        job_link='https://selgros.mingle.ro/ro/embed/apply/' + job['publicUid'],
+        city=city,
+        country='Romania',
+        company=company,
+    ))
 
-        finaljobs.append({
-            "id": str(id),
-            "job_title": job_title,
-            "job_link": job_link,
-            "company": company.get("company"),
-            "country": "Romania",
-            "city": city
-        })
+for version in [1,4]:
+    publish(version, company, jobs, 'APIKEY')
 
-
-#Afisam numarul total de joburi
-print(json.dumps(finaljobs, indent=4))
-
-#Incarcam datele in baza de date
-loadingData(finaljobs, company.get("company"))
+publish_logo(company, 'https://www.selgros.ro/themes/contrib/garnet/dist/assets/branding/logo-selgros.svg')
+show_jobs(jobs)
