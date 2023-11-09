0fc9c1fbb13bc09c51f869fbf1b776af822e30ff
David <david.mondoc@gmail.com>
2023-11-09 21:51:01 +0200

    adaugat judet in weatherford

--- sites/weatherford.py
+++ sites/weatherford.py
@@ -1,5 +1,7 @@
 from scraper.Scraper import Scraper
 from utils import (create_job, publish, publish_logo, show_jobs)
+from getCounty import get_county
+
 
 url = "https://external-weatherford.icims.com/jobs/search?ss=1&searchRelation=keyword_all&searchLocation=13526--&mobile=false&width=1424&height=500&bga=true&needsRedirect=false&jan1offset=120&jun1offset=180&in_iframe=1"
 
@@ -13,16 +15,33 @@ jobs_elements = scraper.find("div", class_="iCIMS_JobsTable").find_all("div", cl
 
 for job in jobs_elements:
     if job.find("div", {'class':'title'}):
-        jobs.append(create_job(
-            company = company,
-            job_title = job.find("div", class_="title").find("a",{'class':'iCIMS_Anchor'}).text.strip(),
-            job_link = job.find("div", class_="title").find("a")["href"],
-            country = "Romania",
-            city = "Ploiesti"
-        ))
+        job_title = job.find("div", class_="title").find("a", {'class': 'iCIMS_Anchor'}).text.strip()
+        job_link = job.find("div", class_="title").find("a")["href"]
+        country = "Romania"
+        city = job.find('div', {'class': 'header'}).find_all('span')[-1].text.strip().split("|")
+        cities=[]
+        counties=[]
+        for county in city:
+            city = county.replace('RO-','').strip().capitalize()
+            if city == "Cimpina":
+                city = "Campina"
+            judet=get_county(city)
+            if judet and judet not in counties:
+                counties.append(judet)
+            if judet and city not in cities:
+                cities.append(city)
+        if cities and counties:
+            jobs.append(create_job
+                (job_title= job_title,
+                job_link= job_link,
+                city=cities,
+                county=counties,
+                country=country,
+                company=company )
+            )
 
 for version in [1, 4]:
-    publish(version, company, jobs, "APIKEY")
+   publish(version, company, jobs, "APIKEY")
 
 publish_logo(company, "https://www.weatherford.com/Content/Images/logo-weatherford-text.png")
 show_jobs(jobs)
