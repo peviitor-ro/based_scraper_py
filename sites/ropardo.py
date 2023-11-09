727687948caa0a015361955171a52f44dd04f2f6
David <david.mondoc@gmail.com>
2023-11-09 20:05:47 +0200

    .

--- sites/ropardo.py
+++ sites/ropardo.py
@@ -1,6 +1,6 @@
 from scraper.Scraper import Scraper
 from utils import (publish, publish_logo, create_job, show_jobs)
-from getCounty import get_county, remove_diacritics
+from getCounty import get_county
 
 url='https://jobs.ropardo.ro/'
 company='Ropardo'
