ae531fb3c268af64211fdfca2b0fa38322db39f6
Baluta Laurentiu Marian <contact@laurentiumarian.ro>
2023-11-10 08:06:12 +0200

    Delete sites/unicredittiriac.py

--- sites/unicredittiriac.py
+++
@@ -1,8 +0,0 @@
-from scraper.Scraper import Scraper
-from utils import (publish, publish_logo, create_job, show_jobs)
-from getCounty import get_county
-
-url=''
-company='UnicreditTiriac'
-scraper=Scraper()
-scraper.get_from_url(url)
