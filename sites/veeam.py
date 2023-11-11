cf9b2168157df71b17f3f4dec91eb61d3dc925c9
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-11 17:01:22 +0200

    Update Veeam scraper

--- sites/veeam.py
+++ sites/veeam.py
@@ -13,27 +13,36 @@ scraper.get_from_url(url, "JSON")
 jobs_elements = scraper.markup
 
 for job in jobs_elements:
+    job_title=job["title"]
+    job_link="https://jobs.smartrecruiters.com/Veeam2/" + job["remoteId"]
+    country = ""
+    citys = []
+    countys = []
     for location in job["location"]:
-        country = location["country"]
         city = location["city"]
+        country = location["country"]
 
-        jobObj = create_job(
-            job_title=job["title"],
-            job_link="https://jobs.smartrecruiters.com/Veeam2/" + job["remoteId"],
-            city=city,
-            country=country,
-            company=company
-        )
+        citys.append(city)
 
         if country == "Romania":
-            jobObj["job_link"] = job["applyUrl"]
-            county = get_county(translate_city(city))
-            jobObj["county"] = county
+            citys = []
+            job_link = job["applyUrl"]
+            citys.append(translate_city(city))
+            countys.append(get_county(translate_city(city)))
+    
+    jobObj = create_job(
+        job_title=job_title,
+        job_link=job_link,
+        company=company,
+        country=country,
+        city=citys,
+        county=countys,
+    )
 
-        jobs.append(jobObj)
+    jobs.append(jobObj)
 
 for version in [1,4]:
     publish(version, company, jobs, 'APIKEY')
 
 publish_logo(company, 'https://img.veeam.com/careers/logo/veeam/veeam_logo_bg.svg')
-show_jobs(jobs)  
+show_jobs(jobs) 
