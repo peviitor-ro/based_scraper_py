639023184836b50cca745126320f917553473097
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-11 14:05:46 +0200

    Update dedeman.py

--- sites/dedeman.py
+++ sites/dedeman.py
@@ -2,34 +2,37 @@ import requests
 import json
 from utils import *
 
-url = 'https://recrutare.dedeman.ro/sinapsi-jobannounceswithfilter/'
+url = 'https://recrutare.dedeman.ro/api/sinapsi/jobs'
 company = 'DEDEMAN'
 
 data = {
     'request': {
         'JobAnnounces': [
             {
-                'Id': '',
-                'Function': '',
-                'City': '',
+                'Id': 'specific_id_value',
+                'Function': 'specific_function_value',
+                'City': 'specific_city_value',
+                'WorkingPoint': 'specific_point_value'
             }
         ]
     }
 }
 
 response_data = requests.post(url, json=data).json()
-job_announces = response_data['d']['JobAnnounces']
 
-final_jobs = []
+if 'd' in response_data and 'JobAnnounces' in response_data['d']:
+    job_announces = response_data['d']['JobAnnounces']
 
-for job in job_announces:
-    final_jobs.append(
+    final_jobs = []
+
+    for job in job_announces:
+        final_jobs.append(
             create_job(
-                job_title = job['Function'],
-                company = company,
-                city = job['City'],
-                country = 'Romania',
-                job_link = 'https://recrutare.dedeman.ro/detalii-post.php?id=' + job['Id']
+                job_title=job['Function'],
+                company=company,
+                city=job['City'],
+                country='Romania',
+                job_link='https://recrutare.dedeman.ro/detalii-post?id=' + job['Id']
             )
         )
 
@@ -37,5 +40,3 @@ for version in [1, 4]:
     publish(version, company, final_jobs, 'Grasum_Key')
 
 publish_logo(company, 'https://i.dedeman.ro/dedereact/design/images/logo.svg')
-
-print(json.dumps(final_jobs, indent=4, sort_keys=True)) 
