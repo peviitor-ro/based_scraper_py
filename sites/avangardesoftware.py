b83f5380f8df47846656e3d960334a295599b06b
TheMrM <grasu_xxl_doja@yahoo.com>
2023-11-14 14:05:39 +0200

    Removed comments

--- sites/avangardesoftware.py
+++ sites/avangardesoftware.py
@@ -42,13 +42,13 @@ for a_tag in a_tags:
                        'country': 'Romania',
                        'company': company})
 
-#api_key = os.environ.get('Grasum_Key')
+api_key = os.environ.get('Grasum_Key')
 
-#clean_data = requests.post('https://api.peviitor.ro/v4/clean/', headers={'Content_Type': 'application/x-www-form-urlencoded', 'apikey': api_key}, data={'company': company})
+clean_data = requests.post('https://api.peviitor.ro/v4/clean/', headers={'Content_Type': 'application/x-www-form-urlencoded', 'apikey': api_key}, data={'company': company})
 
-#update_data = requests.post('https://api.peviitor.ro/v4/update/', headers={'Content_Type': 'application/json', 'apikey': api_key}, data=json.dumps(final_jobs))
+update_data = requests.post('https://api.peviitor.ro/v4/update/', headers={'Content_Type': 'application/json', 'apikey': api_key}, data=json.dumps(final_jobs))
 
-#requests.post('https://api.peviitor.ro/v1/logo/add/', headers={'Content_Type': 'application/json'}, data=json.dumps([{'id': company, 'logo': 'https://avangarde-software.com/wp-content/uploads/2020/03/Avangarde-Software-Logo-1.png'}]))
+requests.post('https://api.peviitor.ro/v1/logo/add/', headers={'Content_Type': 'application/json'}, data=json.dumps([{'id': company, 'logo': 'https://avangarde-software.com/wp-content/uploads/2020/03/Avangarde-Software-Logo-1.png'}]))
 
 print(json.dumps(final_jobs, indent=4))
 
--- sites/tremend.py
+++ sites/tremend.py
@@ -38,9 +38,9 @@ for job in job_elements.find_all('div', class_='career-wrapper'):
         )
     )
 
-#for version in [1, 4]:
-    #publish(version, company, final_jobs, 'Grasum_Key')
+for version in [1, 4]:
+    publish(version, company, final_jobs, 'Grasum_Key')
 
-#publish_logo(company, 'https://www.drupal.org/files/styles/grid-4-2x/public/logo%20Tremend%20480%20x480.png')
+publish_logo(company, 'https://www.drupal.org/files/styles/grid-4-2x/public/logo%20Tremend%20480%20x480.png')
 
 print(json.dumps(final_jobs, indent=4))
