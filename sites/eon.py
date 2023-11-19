ade8c2d3678df434fe9544555a4990e27080f29b
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-19 20:14:10 +0200

    Fixed Eon scraper

--- sites/eon.py
+++ sites/eon.py
@@ -1,12 +1,14 @@
 from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
 import json
+from utils import (translate_city, acurate_city_and_county,)
+from getCounty import (get_county, remove_diacritics)
 
-#Folosim ScraperSelenium deoarece numarul de joburi este incarcat prin AJAX
-scraper = Scraper("https://careers.eon.com/romania/go/Toate-joburile-din-Romania/3727401?utm_source=pagina-cariere-ro")
+# Folosim ScraperSelenium deoarece numarul de joburi este incarcat prin AJAX
+scraper = Scraper(
+    "https://careers.eon.com/romania/go/Toate-joburile-din-Romania/3727401?utm_source=pagina-cariere-ro")
 rules = Rules(scraper)
 
-#Luam numarul total de joburi
+# Luam numarul total de joburi
 jobs = rules.getTag("span", {"class": "paginationLabel"}).find_all("b")[1]
 step = 25
 
@@ -16,7 +18,12 @@ totalJobs = [*range(0, int(jobs.text), step)]
 company = {"company": "Eon"}
 finalJobs = list()
 
-#Pentru fiecare pagina, luam joburile si le adaugam in lista finalJobs
+acurate_city = acurate_city_and_county(
+    Iasi={"city": "Iasi", "county": "Iasi"},
+    Targu_Mures={"city": "Targu-Mures", "county": "Mures"}
+)
+
+# Pentru fiecare pagina, luam joburile si le adaugam in lista finalJobs
 for page in totalJobs:
     pageurl = f"https://careers.eon.com/romania/go/Toate-joburile-din-Romania/3727401/{page}/?q=&sortColumn=referencedate&sortDirection=desc"
     pageScaper = Scraper(pageurl)
@@ -25,22 +32,41 @@ for page in totalJobs:
     jobs = rules.getTags("tr", {"class": "data-row"})
 
     for job in jobs:
-        id = uuid.uuid4()
         job_title = job.find("a").text
         job_link = "https://careers.eon.com" + job.find("a")["href"]
-        city = job.find("span", {"class": "jobLocation"}).text.split(",")[0]
+        city = translate_city(
+            remove_diacritics(
+                job.find("span", {"class": "jobLocation"}
+                         ).text.split(",")[0].strip()
+            )
+        )
 
-        finalJobs.append({
-            "id": str(id),
+        job = {
             "job_title": job_title,
             "job_link": job_link,
-            "company": company.get("company"),
-            "country": "Romania",
-            "city": city.strip(),
-        })
+        }
 
-#Afisam numarul de joburi extrase
+        if acurate_city.get(city.replace(" ", "_")):
+            county = acurate_city.get(city.replace(" ", "_")).get("county")
+            city = acurate_city.get(city.replace(" ", "_")).get("city")
+
+            job["city"] = city
+            job["county"] = county
+
+        elif "Remote" in city:
+            job["remote"] = ["Remote"]
+
+        else:
+            county = get_county(city)
+            if not county:
+                county = get_county(city.replace(" ", "-"))
+            job["city"] = city
+            job["county"] = county
+
+        finalJobs.append(job)
+
+# Afisam numarul de joburi extrase
 print(json.dumps(finalJobs, indent=4))
 
-#Incarcam joburile in baza de date
+# Incarcam joburile in baza de date
 loadingData(finalJobs, company.get("company"))
