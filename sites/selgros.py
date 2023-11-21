95b54f041e4bc5a539eee98196238017731d2c53
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-22 01:05:15 +0200

    fixet Selgros scraper

--- sites/selgros.py
+++ sites/selgros.py
@@ -1,5 +1,6 @@
 from scraper.Scraper import Scraper
 from utils import (publish, publish_logo, create_job, show_jobs)
+from getCounty import get_county
 
 company = 'Selgros'
 url = 'https://mingle.ro/api/boards/mingle/jobs?q=companyUid~eq~%22selgros%22&page=0&pageSize=1000&sort=modifiedDate~DESC'
@@ -7,21 +8,49 @@ url = 'https://mingle.ro/api/boards/mingle/jobs?q=companyUid~eq~%22selgros%22&pa
 scraper = Scraper()
 scraper.get_from_url(url, "JSON")
 
+acurate_city = {
+    "Iasi": {
+        "city": "Iasi",
+        "county": "Iasi"
+    },
+}
+
 jobs = []
 
 for job in scraper.markup['data']['results']:
     try:
-        city = job['locations'][0]['name']
-    except:
-        city = 'Romania'
+        locations = job['locations']
+        cities = [] 
+        counties = []
+        for city in locations:
+            if "Bucuresti" in city["name"]:
+                if "Bucuresti" not in cities:
+                    cities.append("Bucuresti")
+                    counties.append("Bucuresti")
+            else:
+                
+                county = get_county(city["name"])
 
-    jobs.append(create_job(
-        job_title=job['jobTitle'],
-        job_link='https://selgros.mingle.ro/ro/embed/apply/' + job['publicUid'],
-        city=city,
-        country='Romania',
-        company=company,
-    ))
+                if county and county not in counties:
+                    cities.append(city["name"])
+                    counties.append(county)
+                else:
+                    county = get_county(
+                        city["name"].replace(" ", "-")
+                    )
+                    if county and county not in counties:
+                        cities.append(city["name"].replace(" ", "-"))
+                        counties.append(county)
+        jobs.append(create_job(
+            job_title=job['jobTitle'],
+            job_link='https://selgros.mingle.ro/ro/embed/apply/' + job['publicUid'],
+            city=cities,
+            country='Romania',
+            company=company,
+            county=counties,
+        ))
+    except Exception as e:
+        pass
 
 for version in [1,4]:
     publish(version, company, jobs, 'APIKEY')
