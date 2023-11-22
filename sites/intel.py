384a0454f59ae72e3981f711a8b297e891891f99
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-22 13:23:06 +0200

    Fixed Intel Scraper

--- sites/intel.py
+++ sites/intel.py
@@ -1,48 +1,76 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
-import json
+from scraper.Scraper import Scraper
+from getCounty import remove_diacritics, get_county
+from utils import (publish, publish_logo,
+                   show_jobs, translate_city)
+
+
+def get_aditional_city(url):
+    scraper = Scraper()
+    scraper.get_from_url(url)
+
+    locations = scraper.find(
+        'span', {'class': 'job-description__location-pin'}).text.split('|')
+
+    cities = []
+    counties = []
+
+    for location in locations:
+        city = remove_diacritics(
+            translate_city(
+                location.split(',')[0].strip()
+            )
+        )
+        county = get_county(city)
+
+        if county:
+            cities.append(city)
+            counties.append(county)
+
+    return cities, counties
+
 
 url = "https://jobs.intel.com/en/search-jobs/Romania/599/2/798549/46/25/50/2"
 
-company = {"company": "Intel"}
+company = "Intel"
 finalJobs = list()
 
 scraper = Scraper()
-scraper.session.headers.update({
+scraper.set_headers({
     "Accept-Language": "en-GB,en;q=0.9",
 })
 
-scraper.url = url
-rules = Rules(scraper)
+scraper.get_from_url(url)
 
-jobs = rules.getTag("section", {"id": "search-results-list"}).findAll("li")
+jobs = scraper.find("section", {"id": "search-results-list"}).findAll("li")
 
 for job in jobs:
-    id = uuid.uuid4()
     job_title = job.find("h2").text.strip()
     job_link = "https://jobs.intel.com" + job.find("a").get("href")
 
+    cities = job.find("span", {"class": "job-location"}).text.strip()
+    if "Multiple Locations" in cities:
+        cities, counties = get_aditional_city(job_link)
+    else:
+        locations = cities.split(",")
+        cities = remove_diacritics(
+            locations[0].strip()
+        )
+        counties = remove_diacritics(
+            locations[1].strip()
+        )
+
     finalJobs.append({
-        "id": str(id),
         "job_title": job_title,
         "job_link": job_link,
-        "company": company.get("company"),
+        "company": company,
         "country": "Romania",
-        "city": "Romania"
+        "city": cities,
+        "county": counties
     })
 
-print(json.dumps(finalJobs, indent=4))
-
-loadingData(finalJobs, company.get("company"))
+for version in [1, 4]:
+    publish(version, company, finalJobs, 'APIKEY')
 
 logoUrl = "https://tbcdn.talentbrew.com/company/599/gst-v1_0/img/logo/logo-intel-blue.svg"
-
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
-    {
-        "id":company.get("company"),
-        "logo":logoUrl
-    }
-]))
+publish_logo(company, logoUrl)
+show_jobs(finalJobs)
