74a0b8eb35e2f296139909d92e54c608744e576c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-26 00:37:41 +0200

    Fixed Thought Works scraper

--- sites/thoughtworks.py
+++ sites/thoughtworks.py
@@ -1,6 +1,7 @@
 from scraper_peviitor import Scraper, loadingData
-import uuid
 import json
+from utils import translate_city, acurate_city_and_county, create_job
+from getCounty import get_county
 
 url = "https://www.thoughtworks.com/rest/careers/jobs"
 
@@ -11,23 +12,42 @@ scraper = Scraper(url)
 
 jobs = scraper.getJson().get("jobs")
 
+acurate_city = acurate_city_and_county(
+    Cluj={
+        "city": "Cluj-Napoca",
+        "county": "Cluj"
+    },
+    Iasi={
+        "city": "Iasi",
+        "county": "Iasi"
+    },
+)
+
 for job in jobs:
     country = job.get("country")
 
     if country == "Romania":
-        id = uuid.uuid4()
         job_title = job.get("name")
         job_link = "https://www.thoughtworks.com/careers/jobs/" + str(job.get("sourceSystemId"))
-        city = job.get("location")
+        city = translate_city(
+            job.get("location").split("-")[0].strip()
+        )
 
-        finalJobs.append({
-            "id": str(id),
-            "job_title": job_title,
-            "job_link": job_link,
-            "country": country,
-            "city": city,
-            "company": company.get("company")
-        })
+        job = create_job(
+            job_title=job_title,
+            job_link=job_link,
+            company=company.get("company"),
+            country=country
+        )
+
+        if city in acurate_city.keys():
+            job["city"] = acurate_city.get(city).get("city")
+            job["county"] = acurate_city.get(city).get("county")
+        else:
+            job["city"] = city
+            job["county"] = get_county(city)
+
+        finalJobs.append(job)
 
 print(json.dumps(finalJobs, indent=4))
 
