b164825c267dcf82f41abb4136e35ae5bc234f81
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-11-30 07:46:20 +0200

    Fixed Banca Transilvania Scraper

--- sites/bancatransilvania.py
+++ sites/bancatransilvania.py
@@ -1,5 +1,6 @@
 from scraper.Scraper import Scraper
-from utils import (publish, publish_logo, create_job, show_jobs, translate_city, acurate_city_and_county)
+from utils import (publish, publish_logo, create_job,
+                   show_jobs, translate_city)
 from getCounty import get_county
 
 company = 'BANCATRANSILVANIA'
@@ -12,12 +13,14 @@ scraper.get_from_url(url, 'JSON')
 
 response = scraper.markup.get('jobs')
 
+
 def get_aditional_city(url):
     scraper = Scraper()
     scraper.get_from_url(url)
 
-    locations = scraper.find('meta', {'data-hid': 'cXenseParse:b19-ejobs_city'})['content'].split(',')
-    
+    locations = scraper.find(
+        'meta', {'data-hid': 'cXenseParse:b19-ejobs_city'})['content'].split(',')
+
     cities = []
     counties = []
 
@@ -25,34 +28,46 @@ def get_aditional_city(url):
         city = translate_city(location)
         county = get_county(city)
 
+        if not county:
+            city = location.replace(' ', '-')
+            county = get_county(city)
+
         cities.append(city)
         counties.append(county)
 
     return cities, counties
-    
+
 
 for job in response:
     job_title = job.get('title')
-    job_link = 'https://www.ejobs.ro/user/locuri-de-munca/' + job.get('slug') + '/' + str(job.get('id'))
+    job_link = 'https://www.ejobs.ro/user/locuri-de-munca/' + \
+        job.get('slug') + '/' + str(job.get('id'))
     country = 'Romania'
     locations = job.get('locations')
-    
+
     cities = []
     counties = []
 
     for location in locations:
         try:
-            city = translate_city(location.get('address').split(',')[0].strip())
+            city = translate_city(location.get(
+                'address').split(',')[0].strip())
             county = get_county(city)
+
+            if not county:
+                city = location.get('address').split(
+                    ',')[0].strip().replace(' ', '-')
+                county = get_county(city)
 
             cities.append(city)
             counties.append(county)
         except:
-            pass
+            cities = []
+            counties = []
 
-    if not cities:    
+    if not cities:
         cities, counties = get_aditional_city(job_link)
-           
+
     jobs.append(create_job(
         job_title=job_title,
         job_link=job_link,
@@ -62,9 +77,9 @@ for job in response:
         county=counties,
     ))
 
-for version in [1,4]:
+for version in [1, 4]:
     publish(version, company, jobs, 'APIKEY')
-publish_logo(company, 'https://www.bancatransilvania.ro/themes/bancatransilvania/assets/images/logos/bt-cariere.svg')
+publish_logo(
+    company, 'https://www.bancatransilvania.ro/themes/bancatransilvania/assets/images/logos/bt-cariere.svg')
 
 show_jobs(jobs)
-
