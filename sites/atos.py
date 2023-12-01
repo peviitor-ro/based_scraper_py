da0b95ef7f434431d874e2d5720359993414ab8c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-02 00:31:34 +0200

    Fixed Atos scraper

--- sites/atos.py
+++ sites/atos.py
@@ -1,52 +1,82 @@
-from scraper_peviitor import Scraper, loadingData, Rules
-import uuid
-import json
+from scraper.Scraper import Scraper
+from utils import translate_city, publish, publish_logo, show_jobs
+from getCounty import get_county, remove_diacritics
+
+
+def get_aditional_city(url):
+    scraper = Scraper()
+    scraper.get_from_url(url)
+
+    locations = scraper.find_all(
+        'span', {'class': 'jobGeoLocation'})
+
+    cities = []
+    counties = []
+
+    for location in locations:
+        city = remove_diacritics(
+            translate_city(
+                location.text.split(',')[0].strip()
+            )
+        )
+        county = get_county(city)
+
+        if county and county not in counties:
+            cities.append(city)
+            counties.append(county)
+
+    return cities, counties
+
 
 url = "https://jobs.atos.net/go/Jobs-in-Romania/3686501/0/?q=&sortColumn=referencedate&sortDirection=desc"
 
-company = {"company": "Atos"}
+company = "Atos"
 finalJobs = list()
 
 scraper = Scraper(url)
-rules = Rules(scraper)
+scraper.get_from_url(url)
 
-totalJobs = int(rules.getTag("span", {"class": "paginationLabel"}).find_all("b")[-1].text.strip())
+totalJobs = int(scraper.find(
+    "span", {"class": "paginationLabel"}).find_all("b")[-1].text.strip())
 
 paginate = [*range(0, totalJobs, 50)]
 
-for page in paginate:
-    url = f"https://jobs.atos.net/go/Jobs-in-Romania/3686501/{page}/?q=&sortColumn=referencedate&sortDirection=desc"
-    scraper.url = url
+jobs = scraper.find("table", {"id": "searchresults"}).find(
+    "tbody").find_all("tr")
 
-    jobs = rules.getTag("table", {"id": "searchresults"}).find("tbody").find_all("tr")
+for page in paginate:
 
     for job in jobs:
-        id = uuid.uuid4()
         job_title = job.find("a").text.strip()
         job_link = "https://jobs.atos.net" + job.find("a").get("href")
-        city = job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip()
+        city = translate_city(
+            job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip())
+        county = get_county(city)
 
-        finalJobs.append({
-            "id": str(id),
+        job_element = {
             "job_title": job_title,
             "job_link": job_link,
             "country": "Romania",
-            "city": city,
-            "company": company.get("company")
-        })
+            "city": [city],
+            "county": [county],
+            "company": company
+        }
 
-print(json.dumps(finalJobs, indent=4))
+        if not county:
+            city, county = get_aditional_city(job_link)
+            job_element["city"] = city
+            job_element["county"] = county
 
-loadingData(finalJobs, company.get("company"))
+        finalJobs.append(job_element)
 
+    url = f"https://jobs.atos.net/go/Jobs-in-Romania/3686501/{page}/?q=&sortColumn=referencedate&sortDirection=desc"
+    scraper.get_from_url(url)
+    jobs = scraper.find("table", {"id": "searchresults"}).find(
+        "tbody").find_all("tr")
+
+for version in [1, 4]:
+    publish(version, company, finalJobs, 'APIKEY')
 logoUrl = "https://rmkcdn.successfactors.com/a7d5dbb6/c9ab6ccb-b086-47f2-b25b-2.png"
 
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
-    {
-        "id":company.get("company"),
-        "logo":logoUrl
-    }
-]))
+publish_logo(company, logoUrl)
+show_jobs(finalJobs)
