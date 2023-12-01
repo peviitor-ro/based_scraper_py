f674855a7191301d1c93db6e13379445b645a76b
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-01 12:10:45 +0200

    Fixed cargill scraper

--- sites/cargill.py
+++ sites/cargill.py
@@ -1,6 +1,33 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
+from scraper.Scraper import Scraper
 import json
+from utils import translate_city
+from getCounty import get_county, remove_diacritics
+
+
+def get_aditional_city(url):
+    scraper = Scraper()
+    scraper.get_from_url(url)
+
+    locations = scraper.find(
+        'span', {'class': 'job-location'}).text.split('|')
+
+    cities = []
+    counties = []
+
+    for location in locations:
+        city = remove_diacritics(
+            translate_city(
+                location.split(',')[0].strip()
+            )
+        )
+        county = get_county(city)
+
+        if county:
+            cities.append(city)
+            counties.append(county)
+
+    return cities, counties
+
 
 url = "https://careers.cargill.com/en/search-jobs/Romania/23251/2/798549/46/25/50/2"
 
@@ -8,27 +35,34 @@ company = {"company": "Cargill"}
 finalJobs = list()
 
 scraper = Scraper()
-scraper.session.headers.update({
+scraper.set_headers({
     "Accept-Language": "en-GB,en;q=0.9",
 })
-scraper.url = url
-rules = Rules(scraper)
+scraper.get_from_url(url)
 
-jobs = rules.getTag("section", {"id": "search-results-list"}).find_all("li")
+jobs = scraper.find("section", {"id": "search-results-list"}).find_all("li")
 
 for job in jobs:
-    id = uuid.uuid4()
     job_title = job.find("h3").text.strip()
     job_link = "https://careers.cargill.com" + job.find("a").get("href")
-    city = job.find("span", {"class": "job-location"}).text.split(",")[0].strip()
+    city = translate_city(
+        remove_diacritics(
+            job.find("span", {"class": "job-location"}
+                     ).text.split(",")[0].strip()
+        )
+    )
+    county = get_county(city)
+
+    if not county:
+        city, county = get_aditional_city(job_link)
 
     finalJobs.append({
-        "id": str(id),
         "job_title": job_title,
         "job_link": job_link,
         "company": company.get("company"),
         "country": "Romania",
-        "city": city
+        "city": city,
+        "county": county,
     })
 
 print(json.dumps(finalJobs, indent=4))
