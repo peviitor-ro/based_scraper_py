3b8563b1f57e2fb64c981a07f9d0b636a0d9b1e0
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-02 14:09:25 +0200

    Fixed dell scraper

--- sites/dell.py
+++ sites/dell.py
@@ -1,6 +1,7 @@
 from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
 import json
+from utils import translate_city
+from getCounty import get_county
 
 url = "https://jobs.dell.com/search-jobs/results?ActiveFacetID=0&RecordsPerPage=1000&Distance=50&RadiusUnitType=0&ShowRadius=False&IsPagination=False&FacetTerm=798549&FacetType=2&FacetFilters%5B0%5D.ID=798549&FacetFilters%5B0%5D.FacetType=2&FacetFilters%5B0%5D.Count=25&FacetFilters%5B0%5D.Display=Romania&FacetFilters%5B0%5D.IsApplied=true&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=3&OrganizationIds=375&ResultsType=0"
 
@@ -19,19 +20,24 @@ rules = Rules(scraper)
 jobs = rules.getTags("li")
 
 for job in jobs:
-    id = uuid.uuid4()
     job_title = job.find("h2").text.strip()
     job_link = "https://jobs.dell.com" + job.find("a").get("href")
-    city = job.find("span", {"class": "job-location-search"}).text.split(",")[0].strip()
-    
-    finaljobs.append({
-        "id": str(id),
+    city = translate_city(job.find("span", {"class": "job-location-search"}).text.split(",")[0].strip())
+
+    job_element = {
         "job_title": job_title,
         "job_link": job_link,
         "country": "Romania",
-        "city": city,
         "company": company.get("company")
-    })
+    }
+
+    if "Remote" in city:
+        job_element["remote"] = "Remote"
+    else:
+        job_element["city"] = city
+        job_element["county"] = get_county(city)
+
+    finaljobs.append(job_element)    
 
 print(json.dumps(finaljobs, indent=4))
 
