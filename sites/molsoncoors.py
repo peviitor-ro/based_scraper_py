1bfed71540aacc2afb1f88aa69fedcc1ee075226
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-02 14:15:17 +0200

    Fixed Molson coors scraper

--- sites/molsoncoors.py
+++ sites/molsoncoors.py
@@ -1,6 +1,7 @@
 from scraper_peviitor import Scraper, loadingData, Rules
-import uuid
 import json
+from utils import translate_city, acurate_city_and_county
+from getCounty import get_county
 
 url = "https://jobs.molsoncoors.com/MolsonCoors_GBSRomania/search/?q=Romania&startrow=1"
 
@@ -10,30 +11,49 @@ finalJobs = list()
 scraper = Scraper(url)
 rules = Rules(scraper)
 
-totalJobs = int(rules.getTag("span", {"class": "paginationLabel"}).find_all("b")[-1].text.strip())
+totalJobs = int(rules.getTag(
+    "span", {"class": "paginationLabel"}).find_all("b")[-1].text.strip())
 
 paginate = [*range(0, totalJobs, 25)]
+
+acurate_city = acurate_city_and_county(
+    Alba={
+        "city": "Alba Iulia",
+        "county": "Alba"
+    }
+)
 
 for page in paginate:
     url = f"https://jobs.molsoncoors.com/MolsonCoors_GBSRomania/search/?q=Romania&startrow={page}"
     scraper.url = url
 
-    jobs = rules.getTag("table", {"id": "searchresults"}).find("tbody").find_all("tr")
+    jobs = rules.getTag("table", {"id": "searchresults"}).find(
+        "tbody").find_all("tr")
 
     for job in jobs:
-        id = uuid.uuid4()
         job_title = job.find("a").text.strip()
         job_link = "https://jobs.molsoncoors.com" + job.find("a").get("href")
-        city = job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip()
+        cities = []
+        counties = []
 
-        finalJobs.append({
-            "id": str(id),
-            "job_title": job_title,
-            "job_link": job_link,
-            "country": "Romania",
-            "city": city,
-            "company": company.get("company")
-        })
+        for city in job.find("span", {"class": "jobLocation"}).text.split(","):
+            city = translate_city(city.strip())
+            if acurate_city.get(city):
+                cities.append(acurate_city.get(city).get("city"))
+                counties.append(acurate_city.get(city).get("county"))
+            elif get_county(city):
+                cities.append(city)
+                counties.append(get_county(city))
+        if cities and counties:
+            finalJobs.append({
+                "job_title": job_title,
+                "job_link": job_link,
+                "country": "Romania",
+                "city": cities,
+                "county": counties,
+                "company": company.get("company")
+            })
+
 
 print(json.dumps(finalJobs, indent=4))
 
