4c2408200bb3662a1a7ab500e3fe6416aaee60da
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-03 02:54:49 +0200

    Fixed Decathlon scraper

--- sites/decathlon.py
+++ sites/decathlon.py
@@ -1,6 +1,7 @@
 from scraper_peviitor import Scraper, loadingData
-import uuid
 import json
+from getCounty import get_county, remove_diacritics
+from utils import translate_city, acurate_city_and_county
 
 #Folosesc selenium deoarece joburile sunt incarcate prin ajax
 scraper = Scraper('https://apply.workable.com//api/v1/widget/accounts/404273')
@@ -9,14 +10,32 @@ jobs = scraper.getJson().get('jobs')
 company = {"company": "Decathlon"}
 finalJobs = list()
 
+acurate_city = acurate_city_and_county(
+    Iasi={
+        'city': 'Iasi',
+        'county': 'Iasi'
+    },
+    Satu_Mare={
+        'city': 'Satu Mare',
+        'county': 'Satu Mare'
+    },
+)
 for job in jobs:
-    id = uuid.uuid4()
     job_title = job.get('title')
     job_link = job.get('url')
-    city = job.get('city')
+    city = translate_city(remove_diacritics(job.get('city')))  
+
+    if acurate_city.get(city.replace(' ', '_')):
+        city = acurate_city.get(city.replace(' ', '_')).get('city')
+        county = acurate_city.get(city.replace(' ', '_')).get('county')
+    else:
+        county = get_county(city)
+    
+    if not county:
+        city = city.replace(' ', '-')
+        county = get_county(city)
 
     finalJobs.append({
-        'id': str(uuid.uuid4()),
         'job_title': job_title,
         'job_link': job_link,
         'company': company.get('company'),
