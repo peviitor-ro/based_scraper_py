68bf8ab0037c4698da03944c840e1faf58ad7816
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-09 16:09:27 +0200

    Fixed 8x8 scraper

--- sites/8x8.py
+++ sites/8x8.py
@@ -1,39 +1,84 @@
-import requests
+from scraper.Scraper import Scraper
+from utils import (publish, publish_logo, create_job,
+                   show_jobs, translate_city)
+from math import ceil
 import json
-from bs4 import BeautifulSoup
-from utils import *
+from getCounty import get_county
 
-url = 'https://jobs.lever.co/8x8'
+
+def get_aditional_city(url):
+    scraper = Scraper()
+    scraper.get_from_url(url, "JSON")
+
+    location = scraper.markup.get("jobPostingInfo").get("location")
+    isremote = scraper.markup.get("jobPostingInfo").get("additionalLocations")
+    remote = []
+
+    if isremote:
+        remote = [city.split("-")[-1] for city in isremote if "Remote" in city]
+
+    city = translate_city(location.split("-")[-1].replace("Office", "").strip())
+    county = get_county(city)
+
+    if not county and city == "Remote":
+        city = ""
+        county = ""
+        remote = ["Remote"]
+
+    return city, county, remote
+
 company = '8x8'
+url = ' https://8x8inc.wd5.myworkdayjobs.com/wday/cxs/8x8inc/8x8_External_Careers/jobs'
 
-response = requests.get(url)
+post_data = {"appliedFacets": {}, "limit": 20,
+             "offset": 0, "searchText": "Romania"}
 
-soup = BeautifulSoup(response.text, 'html.parser')
+headers = {
+    'Content-Type': 'application/json'
+}
 
-job_titles_elements = soup.find_all('h5', attrs={'data-qa': 'posting-name'})
-job_titles = [element.text for element in job_titles_elements]
+jobs = []
 
-job_links_elements = soup.find_all('a', class_='posting-title')
-job_links = [element['href'] for element in job_links_elements]
+scraper = Scraper()
+scraper.set_headers(headers)
+obj = scraper.post(url, json.dumps(post_data))
+step = 20
+total_jobs = obj.json()['total']
+pages = ceil(total_jobs / step)
 
-job_locations = soup.find_all('span', class_='location')
-job_locations = [element.text.split(', ') for element in job_locations]
+for pages in range(0, pages):
+    if pages > 1:
+        post_data['offset'] = pages * step
+        obj = scraper.post(url, json.dumps(post_data))
 
-final_jobs = []
+    for job in obj.json()['jobPostings']:
+        job_title = job['title']
+        job_link = 'https://8x8inc.wd5.myworkdayjobs.com/en-US/8x8_External_Careers' + \
+            job['externalPath']
+        country = 'Romania'
+        remote = []
 
-for job_title, job_link, job_location in zip(job_titles, job_links, job_locations):
-    final_jobs.append(create_job(
-            job_title = job_title,
-            company = company,
-            country = job_location[1],
-            city = job_location[0],
-            job_link = job_link
-        )
-    )
+        city = translate_city(job['locationsText'].split("-")[-1].replace("Office", "").strip())
+        county = get_county(city)
 
-for version in [1,4]:
-    publish(version, company, final_jobs, 'Grasum_Key')
+        if not county:
+            aditional_url = "https://8x8inc.wd5.myworkdayjobs.com/wday/cxs/8x8inc/8x8_External_Careers" + \
+                job.get("externalPath")
+            city, county, remote = get_aditional_city(aditional_url)
 
-publish_logo(company, 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/8x8_square_logo.svg/220px-8x8_square_logo.svg.png')
+        jobs.append(create_job(
+            job_title=job_title,
+            job_link=job_link,
+            country="Romania",
+            city=city,
+            county=county,
+            company=company,
+            remote=remote
+        ))
 
-print(json.dumps(final_jobs, indent=4))
+for version in [1, 4]:
+    publish(version, company, jobs, 'APIKEY')
+
+publish_logo(
+    company, 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/8x8_square_logo.svg/220px-8x8_square_logo.svg.png')
+show_jobs(jobs)
