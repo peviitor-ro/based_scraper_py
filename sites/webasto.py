892ea02deaa185220791c31653b714a61cfcbf71
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-10 18:08:16 +0200

    Fixed webasto scraper

--- sites/webasto.py
+++ sites/webasto.py
@@ -1,6 +1,7 @@
 from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
 import json
+from utils import translate_city
+from getCounty import get_county
 
 url = "https://jobs.webasto.com/search/?createNewAlert=false&q=&optionsFacetsDD_country=RO&optionsFacetsDD_location=&optionsFacetsDD_dept=&optionsFacetsDD_shifttype="
 
@@ -10,35 +11,38 @@ finaljobs = list()
 scraper = Scraper(url)
 rules = Rules(scraper)
 
-jobs = rules.getTag("table", {"id": "searchresults"}).find("tbody").find_all("tr")
+jobs = rules.getTag("table", {"id": "searchresults"}).find(
+    "tbody").find_all("tr")
 
 for job in jobs:
-    id = uuid.uuid4()
     job_title = job.find("a").text.strip()
     job_link = "https://jobs.webasto.com" + job.find("a").get("href")
-    city = job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip()
-
+    city = translate_city(
+        job.find("span", {"class": "jobLocation"}).text.split(",")[0].strip()
+    )
+    county = get_county(city)
+    
     finaljobs.append({
-        "id": str(id),
         "job_title": job_title,
         "job_link": job_link,
         "company": company.get("company"),
         "country": "Romania",
-        "city": city
+        "city": city,
+        "county": county,
     })
 
 print(json.dumps(finaljobs, indent=4))
 
 loadingData(finaljobs, company.get("company"))
 
-logoUrl = "https://www.webasto-career.com/build/images/webasto.png"
+logoUrl = "https://upload.wikimedia.org/wikipedia/commons/8/81/Webasto_20xx_logo.svg"
 
 scraper.session.headers.update({
     "Content-Type": "application/json",
 })
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
+scraper.post("https://api.peviitor.ro/v1/logo/add/", json.dumps([
     {
-        "id":company.get("company"),
-        "logo":logoUrl
+        "id": company.get("company"),
+        "logo": logoUrl
     }
 ]))
