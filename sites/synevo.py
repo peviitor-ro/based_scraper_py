064cea91853b3f4f2f59224981f4a5b297dd7e13
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2023-12-12 12:38:17 +0200

    Fixed Synevo

--- sites/synevo.py
+++ sites/synevo.py
@@ -1,51 +1,65 @@
 from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
 import json
+from getCounty import get_county, remove_diacritics
+from utils import translate_city
 
-#Cream o instanta a clasei Scraper
+# Cream o instanta a clasei Scraper
 scraper = Scraper("https://www.synevo.ro/cariere/")
 rules = Rules(scraper)
 
-#Luam toate categoriile de joburi
+# Luam toate categoriile de joburi
 jobsCategory = rules.getTags("div", {"class": "job-cat-post"})
 
 company = {"company": "Synevo"}
 finaljobs = list()
 
-#Pentru fiecare categorie de joburi luam linkul si mergem pe pagina
+# Pentru fiecare categorie de joburi luam linkul si mergem pe pagina
 for jobCategory in jobsCategory:
-    #Luam linkul categoriei de joburi
+    # Luam linkul categoriei de joburi
     jobCategoryLink = jobCategory.find("a")["href"]
-    #Mergem pe pagina categoriei de joburi
+    # Mergem pe pagina categoriei de joburi
     scraper.url = jobCategoryLink
 
-    #Luam toate joburile din categoria respectiva
+    # Luam toate joburile din categoria respectiva
     job_link = rules.getTags("a", {"class": "jobs-link"})
 
     for job in job_link:
-        #Mergem pe pagina jobului
+        # Mergem pe pagina jobului
         scraper.url = job["href"]
-        #Luam titlul jobului si orasele in care se poate lucra
+        # Luam titlul jobului si orasele in care se poate lucra
         job_title = rules.getTag("h1", {"class": "entry-title"}).text
-        #Daca sunt mai multe orase le impartim in lista
-        try :
-            citys = rules.getTag("div", {"class": "jobs-info-city"}).find("b").text.split(",")
-        except:
-            citys = [rules.getTag("div", {"class": "jobs-info-city"}).find("b").text]
+        # Daca sunt mai multe orase le impartim in lista
+        # try :
+        locations = rules.getTag(
+            "div", {"class": "jobs-info-city"}).find("b").text.split(",")
 
-        #Cream un dictionar cu jobul si il adaugam in lista finala
-        for city in citys:
-            finaljobs.append({
-                "id": str(uuid.uuid4()),
-                "job_title": job_title,
-                "job_link": scraper.url,
-                "company": company.get("company"),
-                "country": "Romania",
-                "city": city.strip(),
-            })
+        cities = list()
+        counties = set()
 
-#Afisam numarul total de joburi
+        for city in locations:
+            if "Chiajna" in city:
+                city = "Chiajna"
+            else:
+                city = translate_city(
+                    remove_diacritics(city.strip())
+                )
+
+            cities.append(city)
+            counties.add(get_county(city))
+
+        # Cream un dictionar cu jobul si il adaugam in lista finala
+
+        finaljobs.append({
+            "job_title": job_title,
+            "job_link": scraper.url,
+            "company": company.get("company"),
+            "country": "Romania",
+            "city": cities,
+            "county": list(counties),
+        })
+
+# #Afisam numarul total de joburi
 print(json.dumps(finaljobs, indent=4))
 
-#Incarcam datele in baza de date
+# #Incarcam datele in baza de date
 loadingData(finaljobs, company.get("company"))
