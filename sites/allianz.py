8caf1cda2c4368129d12fef822cb2168f7a165e1
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-11 17:49:49 +0200

    Refactor Allianz job scraping code

--- sites/allianz.py
+++ sites/allianz.py
@@ -1,53 +1,50 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import uuid
-import re 
-import json
+from scraper_peviitor import Scraper, Rules
+import re
+from utils import publish, publish_logo, show_jobs
 
-#url-ul paginii
+# url-ul paginii
 url = "https://careers.allianz.com/search/?searchby=location&createNewAlert=false&q=&locationsearch=Romania&optionsFacetsDD_department=&optionsFacetsDD_shifttype=&optionsFacetsDD_customfield3=&optionsFacetsDD_customfield2=&optionsFacetsDD_facility=&optionsFacetsDD_customfield4=&inputSearchValue=Romania&quatFlag=false"
-#Numarul de rezultate de pe pagina
+# Numarul de rezultate de pe pagina
 numberOfResults = 0
 
 company = {"company": "Allianz"}
 finaljobs = list()
 
-#Cream un nou scraper
+# Cream un nou scraper
 scraper = Scraper(url)
-#Cream un nou obiect Rules
+# Cream un nou obiect Rules
 rules = Rules(scraper)
 
 pattern = re.compile(r'jobRecordsFound: parseInt\("(.*)"\)')
-#Luam numarul total de joburi
+# Luam numarul total de joburi
 totalJobs = re.search(pattern, scraper.soup.prettify()).group(1)
-#Cream o lista cu numerele de la 0 la numarul total de joburi
+# Cream o lista cu numerele de la 0 la numarul total de joburi
 queryStrings = [*range(0, int(totalJobs), 25)]
 
 for number in queryStrings:
-    #Setam url-ul paginii
-    scraper.url = url + f"https://careers.allianz.com/tile-search-results?q=&locationsearch=Romania&searchby=location&d=15&startrow={number}"
-    #Luam toate joburile
+    # Setam url-ul paginii
+    scraper.url = (
+        url
+        + f"https://careers.allianz.com/tile-search-results?q=&locationsearch=Romania&searchby=location&d=15&startrow={number}"
+    )
+    # Luam toate joburile
     elements = rules.getTags("li", {"class": "job-tile"})
-    #Pentru fiecare job luam titlul, linkul, compania, tara si orasul
-    for element in elements:
-        id = uuid.uuid4()
-        job_title = element.find("a").text.strip()
-        job_link = "https://careers.allianz.com" + element.find("a").get("href")
-        location = element.find("div", {"class": "location"}).find("div").text.strip()
-        remote = ["Hybrid"]
-
-        finaljobs.append({
-            "id": str(id),
-            "job_title": job_title,
-            "job_link": job_link,
+    finaljobs = [
+        {
+            "job_title": element.find("a").text.strip(),
+            "job_link": "https://careers.allianz.com" + element.find("a").get("href"),
             "company": company.get("company"),
             "country": "Romania",
             "city": "Bucuresti",
             "county": "Bucuresti",
-            "remote": remote
-        })
+            "remote": ["Hybrid"],
+        }
+        for element in elements
+    ]
 
-#Afisam numarul de total de joburi
-print(json.dumps(finaljobs, indent=4))
+publish(4, company.get("company"), finaljobs, "APIKEY")
 
-#Incarcam datele in baza de date
-loadingData(finaljobs, company.get("company"))
+logoUrl = "https://rmkcdn.successfactors.com/cdd11cc7/5d49b267-5aa1-4363-8155-d.svg"
+publish_logo(company.get("company"), logoUrl)
+
+show_jobs(finaljobs)
