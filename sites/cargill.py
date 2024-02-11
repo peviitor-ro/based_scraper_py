6e9228848c74198462e2a40bc8676baee897664a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-12 00:06:43 +0200

    Refactor code and fixed CARTOFISSERIE

--- sites/cargill.py
+++ sites/cargill.py
@@ -1,5 +1,4 @@
 from scraper.Scraper import Scraper
-import json
 from utils import translate_city, publish, publish_logo, show_jobs
 from getCounty import get_county, remove_diacritics
 
@@ -8,18 +7,13 @@ def get_aditional_city(url):
     scraper = Scraper()
     scraper.get_from_url(url)
 
-    locations = scraper.find(
-        'span', {'class': 'job-location'}).text.split('|')
+    locations = scraper.find("span", {"class": "job-location"}).text.split("|")
 
     cities = []
     counties = []
 
     for location in locations:
-        city = remove_diacritics(
-            translate_city(
-                location.split(',')[0].strip()
-            )
-        )
+        city = remove_diacritics(translate_city(location.split(",")[0].strip()))
         county = get_county(city)
 
         if county:
@@ -35,9 +29,11 @@ company = {"company": "Cargill"}
 finalJobs = list()
 
 scraper = Scraper()
-scraper.set_headers({
-    "Accept-Language": "en-GB,en;q=0.9",
-})
+scraper.set_headers(
+    {
+        "Accept-Language": "en-GB,en;q=0.9",
+    }
+)
 scraper.get_from_url(url)
 
 jobs = scraper.find("section", {"id": "search-results-list"}).find_all("li")
@@ -45,28 +41,30 @@ jobs = scraper.find("section", {"id": "search-results-list"}).find_all("li")
 for job in jobs:
     job_title = job.find("h3").text.strip()
     job_link = "https://careers.cargill.com" + job.find("a").get("href")
-    city = [translate_city(
-        remove_diacritics(
-            job.find("span", {"class": "job-location"}
-                     ).text.split(",")[0].strip()
+    city = [
+        translate_city(
+            remove_diacritics(
+                job.find("span", {"class": "job-location"}).text.split(",")[0].strip()
+            )
         )
-    )]
+    ]
     county = [get_county(city) for city in city]
 
     if not county[0]:
         city, county = get_aditional_city(job_link)
 
-    finalJobs.append({
-        "job_title": job_title,
-        "job_link": job_link,
-        "company": company.get("company"),
-        "country": "Romania",
-        "city": city,
-        "county": county,
-    })
+    finalJobs.append(
+        {
+            "job_title": job_title,
+            "job_link": job_link,
+            "company": company.get("company"),
+            "country": "Romania",
+            "city": city,
+            "county": county,
+        }
+    )
 
-for version in [1, 4]:
-    publish(version, company.get("company"), finalJobs, 'APIKEY')
+publish(4, company.get("company"), finalJobs, "APIKEY")
 
 logoUrl = "https://tbcdn.talentbrew.com/company/23251/18994/content/logo-full.png"
 
--- utils.py
+++ utils.py
@@ -2,42 +2,63 @@ import requests
 import os
 import json
 
+
 def create_job(**kwargs):
     job = {}
     job.update(kwargs)
     return job
 
+
 def clean(version, company, apikey):
     apikey = os.environ.get(apikey)
     content_type = "application/x-www-form-urlencoded"
-    requests.post("https://api.peviitor.ro/v" + str(version) + "/clean/", headers={"apikey": apikey, "Content-Type": content_type}, data={"company": company})
+    requests.post(
+        "https://api.peviitor.ro/v" + str(version) + "/clean/",
+        headers={"apikey": apikey, "Content-Type": content_type},
+        data={"company": company},
+    )
+
 
 def update(version, apikey, data):
     apikey = os.environ.get(apikey)
     content_type = "application/json"
-    requests.post("https://api.peviitor.ro/v" + str(version) + "/update/", headers={"apikey": apikey, "Content-Type": content_type}, json=data)
+    requests.post(
+        "https://api.peviitor.ro/v" + str(version) + "/update/",
+        headers={"apikey": apikey, "Content-Type": content_type},
+        json=data,
+    )
+
 
 def dataset(company, data):
     content_type = "application/json"
-    requests.post(f"https://dev.laurentiumarian.ro/dataset/based_scraper_py/{company.lower()}.py/", headers={"Content-Type": content_type}, json={"data": len(data)})
+    requests.post(
+        f"https://dev.laurentiumarian.ro/dataset/based_scraper_py/{company.lower()}.py/",
+        headers={"Content-Type": content_type},
+        json={"data": len(data)},
+    )
+
 
 def publish(version, company, data, apikey):
     clean(version, company, apikey)
     update(version, apikey, data)
     dataset(company, data)
 
+
 def publish_logo(company, logo_url):
     content_type = "application/json"
-    requests.post("https://api.peviitor.ro/v1/logo/add/", headers={"Content-Type": content_type}, json=[{
-        "id": company,
-        "logo": logo_url
-    }])
+    requests.post(
+        "https://api.peviitor.ro/v1/logo/add/",
+        headers={"Content-Type": content_type},
+        json=[{"id": company, "logo": logo_url}],
+    )
+
 
 def show_jobs(data):
     print(json.dumps(data, indent=4))
 
+
 def translate_city(city):
-    city = city.replace(' ', '_')
+    city = city.replace(" ", "_")
     cities = {
         # This is general for all scrapers
         "bucharest": "Bucuresti",
@@ -55,13 +76,17 @@ def translate_city(city):
         "campulung_muscel": "Campulung",
         # This is for Strabag
         "petrobrazi": "Ploiesti",
+        # This is for CARTOFISSERIE
+        "Satu-Mare": "Satu Mare",
+        "Piatra_Neamt": "Piatra Neamt",
     }
-    
+
     if cities.get(city.lower()):
         return cities.get(city.lower())
     else:
-        return city.replace('_', ' ')
-    
+        return city.replace("_", " ")
+
+
 def acurate_city_and_county(**kwargs):
     """
     Returns a dictionary containing accurate city and county information.
@@ -72,7 +97,12 @@ def acurate_city_and_county(**kwargs):
     Returns:
     A dictionary containing accurate city and county information.
     """
-    city_and_county = {}
+    city_and_county = {
+        "Satu_Mare": {"city": "Satu Mare", "county": "Satu Mare"},
+        "Iasi": {"city": "Iasi", "county": "Iasi"},
+        "Piatra_Neamt": {"city": "Piatra-Neamt", "county": "Neamt"},
+    }
+
     city_and_county.update(kwargs)
 
     return city_and_county
