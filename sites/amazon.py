8bd2b7ca6090dbfbc4eee22551a3752e80a71dd5
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-11 17:59:47 +0200

    Refactor job creation and add county information

--- sites/amazon.py
+++ sites/amazon.py
@@ -1,48 +1,44 @@
-
 from scraper.Scraper import Scraper
-from utils import (create_job, publish_logo, publish, show_jobs)
+from utils import create_job, publish_logo, publish, show_jobs, translate_city
+from getCounty import get_county
 
-company = 'Amazon'
+company = "Amazon"
 jobs = []
 
 scraper = Scraper()
 offset = 0
 
 while True:
-    url = f'https://www.amazon.jobs/en/search.json?radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset={offset}&result_limit=100&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=Romania&base_query=&city=&country=ROU&region=&county=&query_options=&'
-    scraper.get_from_url(url, type='JSON')
+    url = f"https://www.amazon.jobs/en/search.json?radius=24km&facets%5B%5D=normalized_country_code&facets%5B%5D=normalized_state_name&facets%5B%5D=normalized_city_name&facets%5B%5D=location&facets%5B%5D=business_category&facets%5B%5D=category&facets%5B%5D=schedule_type_id&facets%5B%5D=employee_class&facets%5B%5D=normalized_location&facets%5B%5D=job_function_id&facets%5B%5D=is_manager&facets%5B%5D=is_intern&offset={offset}&result_limit=100&sort=relevant&latitude=&longitude=&loc_group_id=&loc_query=Romania&base_query=&city=&country=ROU&region=&county=&query_options=&"
+    scraper.get_from_url(url, type="JSON")
 
-    jobs_elements = scraper.markup.get('jobs')
+    jobs_elements = scraper.markup.get("jobs")
 
     if len(jobs_elements) == 0:
         break
 
     for job in jobs_elements:
-        city = job.get('city')
-        if city == 'Virtual':
-            jobs.append(
-                create_job(
-                    job_title=job.get('title'),
-                    job_link='https://www.amazon.jobs' + job.get('job_path'),
-                    country='Romania',
-                    remote="Remote",
-                    company=company,
-                    )
-                )
-        else:
-            jobs.append(
-                create_job(
-                    job_title=job.get('title'),
-                    job_link='https://www.amazon.jobs' + job.get('job_path'),
-                    country='Romania',
-                    city=city,
-                    company=company,
-                    )
-                )
-    offset += 100
-            
-for version in [1,4]:
-    publish(version,company,jobs,'APIKEY')
+        city = translate_city(job.get("city"))
+        job_element = create_job(
+            job_title=job.get("title"),
+            job_link="https://www.amazon.jobs" + job.get("job_path"),
+            country="Romania",
+            city=city,
+            county=get_county(city),
+            company=company,
+        )
+        if city == "Virtual":
+            job_element["remote"] = "Remote"
+            del job_element["city"]
+            del job_element["county"]
 
-publish_logo(company, 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/603px-Amazon_logo.svg.png')
+        jobs.append(job_element)
+    offset += 100
+
+publish(4, company, jobs, "APIKEY")
+
+publish_logo(
+    company,
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/603px-Amazon_logo.svg.png",
+)
 show_jobs(jobs)
