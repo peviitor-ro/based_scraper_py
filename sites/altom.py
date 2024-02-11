ee4e4dfbf75836a1cfd25d5a8b16f73db3660200
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-11 17:57:43 +0200

    Refactor altom.py to improve code readability and maintainability

--- sites/altom.py
+++ sites/altom.py
@@ -2,37 +2,32 @@ from scraper.Scraper import Scraper
 from utils import show_jobs, translate_city, publish, publish_logo
 from getCounty import get_county
 
+company = "altom"
 
-def get_data():
-    # URL for the data
-    url = 'https://www.altom.com/jobs/'
+url = "https://www.altom.com/jobs/"
 
-    # Get the data
-    scraper = Scraper()
-    scraper.get_from_url(url)
-    jobs = scraper.find_all('article', class_='job')
+scraper = Scraper()
+scraper.get_from_url(url)
+jobs = scraper.find_all("article", class_="job")
 
-    final_jobs = []
+final_jobs = [
+    {
+        "job_title": job.find("h2", class_="job-title").text.strip(),
+        "job_link": job.find("h2", class_="job-title").find("a").get("href"),
+        "city": translate_city(job.find("p", class_="job-feed-meta").text.strip()),
+        "county": get_county(
+            translate_city(job.find("p", class_="job-feed-meta").text.strip())
+        ),
+        "country": "Romania",
+        "company": company,
+    }
+    for job in jobs
+]
 
-    # Create a list for the data
-    for job in jobs:
-        job_title = job.find('h2', class_='job-title').text.strip()
-        job_link = job.find('h2', class_='job-title').find('a').get('href')
-        job_city = translate_city(job.find('p', class_='job-feed-meta').text.strip())
-        county = get_county(job_city)
-        final_jobs.append({
-            'job_title': job_title,
-            'job_link': job_link,
-            'city': job_city,
-            'county': county,
-            'country': 'Romania',
-            'company': 'altom'})
-    return final_jobs
+publish(4, "altom", final_jobs, "APIKEY")
 
-data = get_data()
-
-for version in [1, 4]:
-    publish(version, 'altom', data, 'Grasum_Key')
-
-publish_logo('altom', 'https://altom.com/app/themes/altom-sage-theme/dist/images/logo-altom_60516779.png')
-show_jobs(data)
+publish_logo(
+    "altom",
+    "https://altom.com/app/themes/altom-sage-theme/dist/images/logo-altom_60516779.png",
+)
+show_jobs(final_jobs)
