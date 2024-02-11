bff70792ac0672a15c117d03652e80fa152a9d5a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-11 23:12:38 +0200

    Update job links and logo URLs

--- sites/capgemini.py
+++ sites/capgemini.py
@@ -1,7 +1,6 @@
-from scraper_peviitor import Scraper, loadingData
-from utils import translate_city, acurate_city_and_county
+from scraper_peviitor import Scraper
+from utils import translate_city, acurate_city_and_county, publish, publish_logo, show_jobs
 from getCounty import get_county
-import json
 
 url = "https://www.capgemini.com/wp-json/macs/v1/jobs?country=ro-en&size=200"
 
@@ -27,20 +26,17 @@ allCounties = [
     "Brasov",
 ]
 
-acurate_city = acurate_city_and_county(
-    Iasi={
-        "city": "Iasi",
-        "county": "Iasi"
-    }
-)
+acurate_city = acurate_city_and_county(Iasi={"city": "Iasi", "county": "Iasi"})
 
 for job in jobs:
     job_title = job.get("title")
-    job_link = "https://www.capgemini.com/ro-en/jobs/" + \
-        job.get("_id") + "/" + job.get("title").replace(" ", "-").lower()
-    city = translate_city(
-        job.get("location")
+    job_link = (
+        "https://www.capgemini.com/ro-en/jobs/"
+        + job.get("_id")
+        + "/"
+        + job.get("title").replace(" ", "-").lower()
     )
+    city = translate_city(job.get("location"))
     county = get_county(city)
 
     job_element = {
@@ -62,18 +58,9 @@ for job in jobs:
 
     finalJobs.append(job_element)
 
-print(json.dumps(finalJobs, indent=4))
-
-loadingData(finalJobs, company.get("company"))
+publish(4, company.get("company"), finalJobs, "APIKEY")
 
 logoUrl = "https://prod.ucwe.capgemini.com/ro-en/wp-content/themes/capgemini2020/assets/images/logo.svg"
+publish_logo(company.get("company"), logoUrl)
 
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post("https://api.peviitor.ro/v1/logo/add/", json.dumps([
-    {
-        "id": company.get("company"),
-        "logo": logoUrl
-    }
-]))
+show_jobs(finalJobs)
