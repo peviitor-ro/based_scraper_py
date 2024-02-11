7da0b7cb78a864f85edf64af4bc77543a6561163
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-11 18:01:38 +0200

    Refactor Arabesque.py to use Scraper class and add new functions

--- sites/arabesque.py
+++ sites/arabesque.py
@@ -1,15 +1,20 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import json
+from scraper.Scraper import Scraper
 from getCounty import get_county
-from utils import translate_city, acurate_city_and_county
+from utils import (
+    translate_city,
+    acurate_city_and_county,
+    publish,
+    publish_logo,
+    show_jobs,
+)
 
 url = "https://cariere.arabesque.ro"
 
-scraper = Scraper(url)
-rules = Rules(scraper)
+scraper = Scraper()
+scraper.get_from_url(url, verify=False)
 
-#Luam toate categoriile de joburi
-categories = rules.getTags("div", {"class": "arabesque_homepage_template_categorie"})
+# Luam toate categoriile de joburi
+categories = scraper.find_all("div", {"class": "arabesque_homepage_template_categorie"})
 
 company = {"company": "Arabesque"}
 finalJobs = list()
@@ -18,27 +23,35 @@ acurate_city = acurate_city_and_county(
     Iasi={"city": "Iasi", "county": "Iasi"},
 )
 
-#Pentru fiecare categorie de joburi
+# Pentru fiecare categorie de joburi
 for category in categories:
-    #Luam joburile din categoria respectiva
+    # Luam joburile din categoria respectiva
     jobs = category.find_all("article")
 
-    #Luam link-ul catre pagina cu joburile din categoria respectiva
-    categoryUrl = category.find("a",{"class", "arabesque_home_go_to"}).get("href").replace("https://cariere.arabesque.ro/jobs/", "")
-    #Cream un link catre pagina cu jobul respectiv
-    jobUrl = "https://cariere.arabesque.ro/jobs/" + "job/" + categoryUrl.replace(" ", "%20")
+    # Luam link-ul catre pagina cu joburile din categoria respectiva
+    categoryUrl = (
+        category.find("a", {"class", "arabesque_home_go_to"})
+        .get("href")
+        .replace("https://cariere.arabesque.ro/jobs/", "")
+    )
+    # Cream un link catre pagina cu jobul respectiv
+    jobUrl = (
+        "https://cariere.arabesque.ro/jobs/" + "job/" + categoryUrl.replace(" ", "%20")
+    )
 
-
-    #Daca sunt mai mult de 4 joburi in categoria respectiva atunci trebuie sa mergem pe pagina cu joburile
+    # Daca sunt mai mult de 4 joburi in categoria respectiva atunci trebuie sa mergem pe pagina cu joburile
     if len(jobs) >= 4:
-        #Luam link-ul catre pagina cu joburile din categoria respectiva
-        urlPage = category.find("a",{"class", "arabesque_home_go_to"}).get("href").replace(" ", "%20")
-        #Se crea un nou scraper pentru pagina cu joburile din categoria respectiva
-        scraperPage = Scraper(urlPage)
-        pageRules = Rules(scraperPage)
-        #Se cauta div-ul cu joburile
-        jobsContainer = pageRules.getTag("div", {"class": "arabesque_homepage_template"})
-        #Se cauta toate joburile
+        # Luam link-ul catre pagina cu joburile din categoria respectiva
+        urlPage = (
+            category.find("a", {"class", "arabesque_home_go_to"})
+            .get("href")
+            .replace(" ", "%20")
+        )
+        # Se crea un nou scraper pentru pagina cu joburile din categoria respectiva
+        scraper.get_from_url(urlPage, verify=False)
+        # Se cauta div-ul cu joburile
+        jobsContainer = scraper.find("div", {"class": "arabesque_homepage_template"})
+        # Se cauta toate joburile
         jobs = jobsContainer.find_all("article")
 
     for job in jobs:
@@ -53,26 +66,30 @@ for category in categories:
             county = acurate_city.get(city).get("county")
 
         elif not county:
-
             first_name = job_title.split(" ")[-2].strip().title()
             city = translate_city(first_name + "-" + city)
             county = get_county(city)
 
             if not county:
-
                 city = "Bucuresti"
                 county = "Bucuresti"
 
-        finalJobs.append({
-            "job_title": job_title,
-            "job_link": job_link,
-            "company": company.get("company"),
-            "country": "Romania",
-            "city": city,
-            "county": county
-        })
+        finalJobs.append(
+            {
+                "job_title": job_title,
+                "job_link": job_link,
+                "company": company.get("company"),
+                "country": "Romania",
+                "city": city,
+                "county": county,
+            }
+        )
 
-print(json.dumps(finalJobs, indent=4))
+publish(4, company.get("company"), finalJobs, "APIKEY")
 
-loadingData(finalJobs, company.get("company"))
+logoUrl = (
+    "https://cariere.arabesque.ro/wp-content/uploads/2016/09/cropped-logo-blog.png"
+)
+publish_logo(company.get("company"), logoUrl)
 
+show_jobs(finalJobs)
