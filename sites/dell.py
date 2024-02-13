00073e943cf9ef1f3460eb1e79d607d2d9a27a51
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-14 00:58:48 +0200

    Refactor Dell.py: Update imports and function calls

--- sites/dell.py
+++ sites/dell.py
@@ -1,6 +1,5 @@
-from scraper_peviitor import Scraper, Rules, loadingData
-import json
-from utils import translate_city
+from scraper_peviitor import Scraper, Rules
+from utils import translate_city, publish, publish_logo, show_jobs
 from getCounty import get_county
 
 url = "https://jobs.dell.com/search-jobs/results?ActiveFacetID=0&RecordsPerPage=1000&Distance=50&RadiusUnitType=0&ShowRadius=False&IsPagination=False&FacetTerm=798549&FacetType=2&FacetFilters%5B0%5D.ID=798549&FacetFilters%5B0%5D.FacetType=2&FacetFilters%5B0%5D.Count=25&FacetFilters%5B0%5D.Display=Romania&FacetFilters%5B0%5D.IsApplied=true&SearchResultsModuleName=Search+Results&SearchFiltersModuleName=Search+Filters&SortCriteria=0&SortDirection=0&SearchType=3&OrganizationIds=375&ResultsType=0"
@@ -22,13 +21,15 @@ jobs = rules.getTags("li")
 for job in jobs:
     job_title = job.find("h2").text.strip()
     job_link = "https://jobs.dell.com" + job.find("a").get("href")
-    city = translate_city(job.find("span", {"class": "job-location-search"}).text.split(",")[0].strip())
+    city = translate_city(
+        job.find("span", {"class": "job-location-search"}).text.split(",")[0].strip()
+    )
 
     job_element = {
         "job_title": job_title,
         "job_link": job_link,
         "country": "Romania",
-        "company": company.get("company")
+        "company": company.get("company"),
     }
 
     if "Remote" in city:
@@ -37,24 +38,11 @@ for job in jobs:
         job_element["city"] = city
         job_element["county"] = get_county(city)
 
-    finaljobs.append(job_element)    
+    finaljobs.append(job_element)
 
-print(json.dumps(finaljobs, indent=4))
-
-loadingData(finaljobs, company.get("company"))
+publish(4, company.get("company"), finaljobs, "Grasum_Key")
 
 logoUrl = "https://tbcdn.talentbrew.com/company/375/v4_0/img/logos/delltech_logo_prm_blue_rgb.jpg"
+publish_logo(company.get("company"), logoUrl)
 
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
-    {
-        "id":company.get("company"),
-        "logo":logoUrl
-    }
-]))
-
-
-
-
+show_jobs(finaljobs)
