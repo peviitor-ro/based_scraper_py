ec385ce3d943e89ef3bc086fa937f2df8fa382ea
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-14 13:13:08 +0200

    Refactor job creation and publishing in fga.py

--- sites/fga.py
+++ sites/fga.py
@@ -1,30 +1,33 @@
 from scraper.Scraper import Scraper
-from utils import (publish, publish_logo, create_job, show_jobs)
+from utils import publish, publish_logo, create_job, show_jobs
 
 url = "https://www.fgaromania.ro/category/cariere/"
 
 company = "fga"
-jobs = []
 
 scraper = Scraper()
 scraper.get_from_url(url)
 
 jobs_elements = scraper.find_all("article", class_="single-hentry")
 
-for job in jobs_elements:
-    job_title=job.find("h2", class_="entry-title").text.strip()
-    if job_title != "Nu sunt poziții deschise în acest moment":
-        jobs.append(create_job(
-            job_title=job_title,
-            job_link=job.find("h2", class_="entry-title").a["href"],
-            company=company,
-            country="Romania",
-            city="Bucuresti",
-            county="Bucuresti",
-        ))
+jobs = [
+    create_job(
+        job_title=job.find("h2", class_="entry-title").text.strip(),
+        job_link=job.find("h2", class_="entry-title").a["href"],
+        company=company,
+        country="Romania",
+        city="Bucuresti",
+        county="Bucuresti",
+    )
+    for job in jobs_elements
+    if job.find("h2", class_="entry-title").text.strip()
+    != "Nu sunt poziții deschise în acest moment"
+]
 
-for version in [1,4]:
-    publish(version, company, jobs, 'APIKEY')
 
-publish_logo(company, 'https://www.fgaromania.ro/wp-content/uploads/2020/06/logo-FGA.png')
+publish(4, company, jobs, "APIKEY")
+
+publish_logo(
+    company, "https://www.fgaromania.ro/wp-content/uploads/2020/06/logo-FGA.png"
+)
 show_jobs(jobs)
