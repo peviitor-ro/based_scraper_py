12142b43c1b76e97985987d41966e5f90bc74431
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-14 13:05:09 +0200

    Refactor ericsson.py: Remove unused imports, filter job locations by county

--- sites/ericsson.py
+++ sites/ericsson.py
@@ -1,5 +1,5 @@
 from scraper.Scraper import Scraper
-from utils import (create_job, clean, update, publish, publish_logo, show_jobs, translate_city)
+from utils import create_job, publish, publish_logo, show_jobs, translate_city
 from getCounty import get_county
 from math import ceil
 
@@ -22,21 +22,28 @@ for page in range(pages):
 
     for job in scraper.markup["positions"]:
         cities = [
-            translate_city(location.split(",")[0]) for location in job["locations"]
+            translate_city(location.split(",")[0])
+            for location in job["locations"]
+            if get_county(translate_city(location.split(",")[0]))
         ]
         counties = [get_county(city) for city in cities]
 
-        jobs.append(create_job(
-            job_title=job["name"],
-            job_link=job["canonicalPositionUrl"],
-            company=company,
-            country="Romania",
-            city=cities,
-            county=counties,
-        ))
+        jobs.append(
+            create_job(
+                job_title=job["name"],
+                job_link=job["canonicalPositionUrl"],
+                company=company,
+                country="Romania",
+                city=cities,
+                county=counties,
+            )
+        )
 
-for version in [1, 4]:
-    publish(version, company, jobs, "APIKEY")
 
-publish_logo(company, "https://logos-world.net/wp-content/uploads/2020/12/Ericsson-Logo-700x394.png")
+publish(4, company, jobs, "APIKEY")
+
+publish_logo(
+    company,
+    "https://logos-world.net/wp-content/uploads/2020/12/Ericsson-Logo-700x394.png",
+)
 show_jobs(jobs)
