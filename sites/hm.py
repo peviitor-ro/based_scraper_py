2385b06bb73648f3f50118033ce5ba2cacc0493c
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-16 00:01:49 +0200

    Refactor HM.py to improve code structure and readability

--- sites/hm.py
+++ sites/hm.py
@@ -1,8 +1,15 @@
-from scraper_peviitor import Scraper, loadingData
-import uuid
-import json
+from scraper_peviitor import Scraper
+from utils import publish, publish_logo, show_jobs
+from getCounty import get_county, remove_diacritics
 
-data = {"locations": [], "workAreas": [], "contractType": [], "fulltext": "Romania", "order_by": "relevance", "page": 1}
+data = {
+    "locations": [],
+    "workAreas": [],
+    "contractType": [],
+    "fulltext": "Romania",
+    "order_by": "relevance",
+    "page": 1,
+}
 url = "https://career.hm.com/wp-json/hm/v1/sr/jobs/search?_locale=user"
 
 # Se creează o instanță a clasei ScraperSelenium pentru a accesa site-ul
@@ -12,29 +19,32 @@ jobs = scraper.post(url, data).json()
 company = {"company": "HM"}
 finalJobs = list()
 
-#din obiectul json extragem lista de job-uri
+# din obiectul json extragem lista de job-uri
 while jobs.get("jobs"):
     for job in jobs.get("jobs"):
-        id = str(uuid.uuid4())
         job_title = job.get("title")
         job_link = job.get("permalink")
-        location = job.get("city")
+        city = remove_diacritics(job.get("city"))
+        county = get_county(city)
 
-        finalJobs.append({
-                "id": id,
+        finalJobs.append(
+            {
                 "job_title": job_title,
                 "job_link": job_link,
                 "company": company.get("company"),
                 "country": "Romania",
-                "city": location,
-            })
+                "city": city,
+                "county": county,
+            }
+        )
 
     # Se trece la pagina următoare
     data["page"] += 1
     jobs = scraper.post(url, data).json()
 
-# Se afișează numărul de job-uri extrase
-print(json.dumps(finalJobs, indent=4))
+publish(4, company.get("company"), finalJobs, "APIKEY")
 
-#Salvarea datelor în baza de date
-loadingData(finalJobs, company.get("company"))
+logourl = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/H%26M-Logo.svg/709px-H%26M-Logo.svg.png?20130107164928"
+publish_logo(company.get("company"), logourl)
+
+show_jobs(finalJobs)
