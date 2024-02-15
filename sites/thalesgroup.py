76a95b51a7831a404837664398a1ff2a29876286
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-16 00:46:04 +0200

    Refactor ThalesGroup scraper code and add job publishing

--- sites/thalesgroup.py
+++ sites/thalesgroup.py
@@ -1,6 +1,6 @@
-from scraper_peviitor import Scraper, loadingData
-import uuid
-import json
+from scraper_peviitor import Scraper
+from utils import publish, publish_logo, show_jobs, translate_city
+from getCounty import get_county
 
 apiUrl = "https://thales.wd3.myworkdayjobs.com/wday/cxs/thales/Careers/jobs"
 
@@ -14,7 +14,12 @@ headers = {
     "Content-Type": "application/json",
 }
 
-data = {"appliedFacets":{"locationCountry":["f2e609fe92974a55a05fc1cdc2852122"]},"limit":20,"offset":0,"searchText":""}
+data = {
+    "appliedFacets": {"locationCountry": ["f2e609fe92974a55a05fc1cdc2852122"]},
+    "limit": 20,
+    "offset": 0,
+    "searchText": "",
+}
 
 scraper.session.headers.update(headers)
 
@@ -26,33 +31,27 @@ for num in iteration:
     data["offset"] = num
     jobs = scraper.post(apiUrl, json=data).json().get("jobPostings")
     for job in jobs:
-        id = uuid.uuid4()
         job_title = job.get("title")
-        job_link = "https://thales.wd3.myworkdayjobs.com/en-US/Careers" + job.get("externalPath")
-        city = job.get("locationsText").split(",")[0]
+        job_link = "https://thales.wd3.myworkdayjobs.com/en-US/Careers" + job.get(
+            "externalPath"
+        )
+        city = translate_city(job.get("locationsText").split(",")[0])
+        county = get_county(city)
 
-        finalJobs.append({
-            "id": str(id),
-            "job_title": job_title,
-            "job_link": job_link,
-            "company": company.get("company"),
-            "country": "Romania",
-            "city": city
-        })
+        finalJobs.append(
+            {
+                "job_title": job_title,
+                "job_link": job_link,
+                "company": company.get("company"),
+                "country": "Romania",
+                "city": city,
+                "county": county,
+            }
+        )
 
-print(json.dumps(finalJobs, indent=4))
-
-loadingData(finalJobs, company.get("company"))
+publish(4, company.get("company"), finalJobs, "APIKEY")
 
 logoUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Thales_Logo.svg/484px-Thales_Logo.svg.png?20210518101610"
+publish_logo(company.get("company"), logoUrl)
 
-scraper.session.headers.update({
-    "Content-Type": "application/json",
-})
-scraper.post( "https://api.peviitor.ro/v1/logo/add/" ,json.dumps([
-    {
-        "id":company.get("company"),
-        "logo":logoUrl
-    }
-]))
-      
+show_jobs(finalJobs)
