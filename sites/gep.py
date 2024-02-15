21a6048054ac931d92f6a525535bf080cf877ff7
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-02-15 23:39:20 +0200

    Refactor job scraping code for GEP and GoodYear

--- sites/gep.py
+++ sites/gep.py
@@ -1,26 +1,31 @@
 from scraper.Scraper import Scraper
-from utils import (publish, publish_logo, create_job, show_jobs)
+from utils import publish, publish_logo, create_job, show_jobs
 
-company = 'GEP'
-url = 'https://jobseurope-gep.icims.com/jobs/search?ss=1&searchRelation=keyword_all&searchLocation=13526&in_iframe=1'
+company = "GEP"
+url = "https://jobseurope-gep.icims.com/jobs/search?ss=1&searchRelation=keyword_all&searchLocation=13526&in_iframe=1"
 
 scraper = Scraper()
 scraper.get_from_url(url)
 
-jobs = []
-jobs_elements = scraper.find('div', class_='iCIMS_JobsTable').find_all('div', class_='row')
+jobs_elements = scraper.find("div", class_="iCIMS_JobsTable").find_all(
+    "div", class_="row"
+)
 
-for job in jobs_elements:
-    jobs.append(create_job(
-        job_title=job.find('div', class_='title').text.strip().replace('Title\n\n', ''),
-        job_link=job.find('a', class_='iCIMS_Anchor')['href'],
-        city='Cluj-Napoca',
-        country='Romania',
+jobs = [
+    create_job(
+        job_title=job.find("div", class_="title").text.strip().replace("Title\n\n", ""),
+        job_link=job.find("a", class_="iCIMS_Anchor")["href"],
+        city="Cluj-Napoca",
+        country="Romania",
         company=company,
-    ))
+    )
+    for job in jobs_elements
+]
 
-for version in [1,4]:
-    publish(version, company, jobs, 'APIKEY')
+publish(4, company, jobs, "APIKEY")
 
-publish_logo(company, 'https://gep.icims.com/icims2/servlet/icims2?module=AppInert&action=download&id=96578&hashed=855987836')
+publish_logo(
+    company,
+    "https://gep.icims.com/icims2/servlet/icims2?module=AppInert&action=download&id=96578&hashed=855987836",
+)
 show_jobs(jobs)
--- sites/goodyear.py
+++ sites/goodyear.py
@@ -1,40 +1,50 @@
 from scraper.Scraper import Scraper
-from utils import (publish, publish_logo, create_job, show_jobs)
+from utils import publish, publish_logo, create_job, show_jobs, translate_city
 from math import ceil
+from getCounty import get_county
 
-company = 'GoodYear'
-url = 'https://jobs.goodyear.com/search/?createNewAlert=false&q=&locationsearch=Romania'
+company = "GoodYear"
+url = "https://jobs.goodyear.com/search/?createNewAlert=false&q=&locationsearch=Romania"
 
 scraper = Scraper()
 scraper.get_from_url(url)
 
-totalJobs = int(scraper.find("span", class_ = "paginationLabel").find_all("b")[-1].text.strip())
+totalJobs = int(
+    scraper.find("span", class_="paginationLabel").find_all("b")[-1].text.strip()
+)
 
 pages = ceil(totalJobs / 25)
 
 jobs = []
 
 for page in range(1, pages + 1):
-    
-    jobs_elements = scraper.find('table', id='searchresults').find('tbody').find_all('tr')
+
+    jobs_elements = (
+        scraper.find("table", id="searchresults").find("tbody").find_all("tr")
+    )
 
     for job in jobs_elements:
-        job_link = 'https://jobs.goodyear.com' + job.find('a')['href']
-        job_title = job.find('a').text.strip()
-        job_location = job.find('span', class_='jobLocation').text.split(',')[0].strip()
+        job_link = "https://jobs.goodyear.com" + job.find("a")["href"]
+        job_title = job.find("a").text.strip()
+        job_location = translate_city(job.find("span", class_="jobLocation").text.split(",")[0].title().strip())
 
-        jobs.append(create_job(
-            job_title=job_title,
-            job_link=job_link,
-            city=job_location,
-            country='Romania',
-            company=company,
-        ))
+        jobs.append(
+            create_job(
+                job_title=job_title,
+                job_link=job_link,
+                city=job_location,
+                county = get_county(job_location),
+                country="Romania",
+                company=company,
+            )
+        )
 
-    scraper.get_from_url(url + f'&startrow={page * 25}')
+    scraper.get_from_url(url + f"&startrow={page * 25}")
 
-for version in [1,4]:
-    publish(version, company, jobs, 'APIKEY')
 
-publish_logo(company, 'https://rmkcdn.successfactors.com/38b5d3dd/ef930ba2-97c9-4abc-a14a-e.png')
+publish(4, company, jobs, "APIKEY")
+
+publish_logo(
+    company, "https://rmkcdn.successfactors.com/38b5d3dd/ef930ba2-97c9-4abc-a14a-e.png"
+)
 show_jobs(jobs)
