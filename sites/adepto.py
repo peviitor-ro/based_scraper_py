73fd186e391838fa648b10bef83152383de8cfa9
ImperaLuna <rares.brezeanu.dev@gmail.com>
2024-02-21 12:57:55 +0200

    changes for Adepto Technical PR

--- sites/adepto.py
+++ sites/adepto.py
@@ -1,6 +1,6 @@
 from scraper.Scraper import Scraper
 from utils import show_jobs, translate_city
-from getCounty import remove remove_diacritics
+from getCounty import remove_diacritics
 
 url = "https://jobs.workable.com/api/v1/companies/3N1Vi5vkmhbtLoUoe4xVH9/"
 logo = "https://workablehr.s3.amazonaws.com/uploads/account/logo/586136/logo"
@@ -16,18 +16,17 @@ final_jobs = []
 for job in jobs:
     if job["location"]["countryName"] == "Romania":
 
-        county = job["location"]["subregion"].replace("ș", "s")
-        county = remove_diacritics(job["location"]["subregion"])
-        city = remove_diacriticsjob["location"]["city"])
+        county = translate_city(remove_diacritics(job["location"]["subregion"]))
+        city = translate_city(remove_diacritics(job["location"]["city"]))
 
         final_jobs.append({
-                           "company": job["company"]["title"],
-                           "job_title": job["title"],
-                           "job_link": job["url"],
-                           "remote": remote,
-                           "country": "Romania",
-                           "county": translate_city(county),
-                           "city": translate_city(city),
-                           })
+                            "company": job["company"]["title"],
+                            "job_title": job["title"],
+                            "job_link": job["url"],
+                            "remote": job["workplace"],
+                            "country": "Romania",
+                            "county": county,
+                            "city": city
+                            })
 
 show_jobs(final_jobs)
