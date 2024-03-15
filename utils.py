05a94a976f650855db85f70dbafe9de2bf3675f0
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-03-15 17:52:46 +0200

    Add API token retrieval and validation

--- utils.py
+++ utils.py
@@ -1,6 +1,22 @@
 import requests
 import os
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
+
+domain = os.environ.get("DOMAIN")
+
+def get_token():
+    """
+    Returnează token-ul necesar pentru a face request-uri către API.
+    :return: token-ul necesar pentru a face request-uri către API
+    """
+    endpoint = os.environ.get("TOKEN_ROUTE")
+    email = os.environ.get("EMAIL")
+    url = f'{domain}{endpoint}'
+    response = requests.post(url, json={"email": email})
+    return response.json()["access"]
 
 
 def create_job(**kwargs):
@@ -27,6 +43,15 @@ def update(version, apikey, data):
         headers={"apikey": apikey, "Content-Type": content_type},
         json=data,
     )
+
+    # VALIDATOR
+    route = os.environ.get("ADD_JOBS_ROUTE")
+    url = f"{domain}{route}"
+    token = os.environ.get("TOKEN") if os.environ.get("TOKEN") else get_token()
+
+    headers = {"Content-Type": "application/json", "Authorization": f"Bearer {token}"}
+
+    requests.post(url, headers=headers, json=data)
 
 
 def dataset(company, data):
@@ -129,4 +154,3 @@ def get_jobtype(sentence, **kwargs):
     ]
 
     return list(set(types))
-
