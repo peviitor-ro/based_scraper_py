0177c2ff138a27eef62fc07899a51967b7eb876a
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2024-04-11 18:55:56 +0300

    Update 1and1.py to use GetCounty class and publish_or_update function

--- sites/1and1.py
+++ sites/1and1.py
@@ -1,6 +1,6 @@
 from scraper_peviitor import Scraper
-from utils import translate_city, publish, publish_logo, show_jobs
-from getCounty import get_county
+from utils import translate_city, publish_logo, show_jobs, publish_or_update
+from getCounty import GetCounty
 import re
 
 url = "https://www.1and1.ro/jobs.json"
@@ -13,6 +13,7 @@ scraper.url = url
 jobs = scraper.getJson().get("jobs")
 
 remote_pattern = re.compile(r"\(.+\)")
+_counties = GetCounty()
 
 finalJobs = [
     {
@@ -20,7 +21,7 @@ finalJobs = [
         "job_link": "https://www.1and1.ro/careers/" + job.get("RefURL"),
         "country": "Romania",
         "city": translate_city(job.get("Location")),
-        "county": get_county(translate_city(job.get("Location"))),
+        "county": _counties.get_county(translate_city(job.get("Location"))),
         "remote": (
             remote_pattern.search(job.get("JobTitle"))
             .group(0)
@@ -37,7 +38,7 @@ finalJobs = [
     for job in jobs
 ]
 
-publish(4, company.get("company"), finalJobs, "APIKEY")
+publish_or_update(finalJobs)
 
 logoUrl = "https://cdn.website-editor.net/b236a61347464e4b904f5e6b661c2af9/dms3rep/multi/1and1-logo.svg"
 publish_logo(company.get("company"), logoUrl)
