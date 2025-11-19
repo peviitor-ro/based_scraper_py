38eabcc6f760f9d263bca2b6e0a7f0e68a738272
lalalaurentiu <laurentiumarianbaluta@gmail.com>
2025-11-19 17:57:42 +0200

    Refactor Danone scraper to enhance job data extraction and update URL handling

--- sites/danone.py
+++ sites/danone.py
@@ -1,30 +1,50 @@
 from scraper.Scraper import Scraper
-from utils import publish_or_update, publish_logo, show_jobs, translate_city
+from utils import publish_or_update, publish_logo, show_jobs, translate_city, get_jobtype
 from getCounty import GetCounty
 
 _counties = GetCounty()
-url = "https://careers.danone.com/bin/jobs.json?countries=Romania&locale=en&limit=100"
+url = "https://careers.danone.com/content/corp/global/career-rebrand/global/en/jobs.results.html?10000_group.propertyvalues.property=jcr%3Acontent%2Fdata%2Fmaster%2Fcountry&10000_group.propertyvalues.operation=equals&10000_group.propertyvalues.66_values=Romania&10004_group.propertyvalues.property=jcr%3Acontent%2Fdata%2Fmaster%2Fcountry&10004_group.propertyvalues.operation=equals&10004_group.propertyvalues.66_values=Romania&layout=teaserList&p.offset=0&p.limit=12"
 
 company = {"company": "Danone"}
 
 
 scraper = Scraper()
-scraper.get_from_url(url, "JSON")
 
-jobs = scraper.markup.get("results")
+scraper.get_from_url(url)
 
-finalJobs = [
-    {
-        "job_title": job.get("title"),
-        "job_link": "https://careers.danone.com/en-global/jobs/" + job.get("url"),
-        "company": company.get("company"),
-        "country": "Romania",
-        "city": translate_city(job.get("city").title()),
-        "county": _counties.get_county(translate_city(job.get("city").title())),
-        "remote": job.get("workFromHome").replace("Field", "")
-    }
-    for job in jobs
-]
+
+jobs = scraper.find_all("div", {"class": "dn-jobdetails__job-card"})
+
+
+finalJobs = []
+
+while jobs:
+    for job in jobs:
+        job_title = job.find("h3", {"class": "job-card__title"}).text.strip()
+        job_url = "https://careers.danone.com" + job.find("a")["href"].strip()
+        location = job.find("h4", {"class": "job-card__city"}).text.strip()
+        city = translate_city(location.split(",")[0].strip())
+        county = _counties.get_county(city)
+        remote = get_jobtype(
+            job.find("h4", {"class": "job-card__workFromHome"}).text.strip()
+        )
+
+        finalJobs.append(
+            {
+                "job_title": job_title,
+                "job_link": job_url,
+                "city": city,
+                "country": "Romania",
+                "county": county,
+                "company": company.get("company"),
+                "remote": remote,
+            }
+        )
+    scraper.get_from_url(
+        f"https://careers.danone.com/content/corp/global/career-rebrand/global/en/jobs.results.html?10000_group.propertyvalues.property=jcr%3Acontent%2Fdata%2Fmaster%2Fcountry&10000_group.propertyvalues.operation=equals&10000_group.propertyvalues.66_values=Romania&10004_group.propertyvalues.property=jcr%3Acontent%2Fdata%2Fmaster%2Fcountry&10004_group.propertyvalues.operation=equals&10004_group.propertyvalues.66_values=Romania&layout=teaserList&p.offset={len(finalJobs)}&p.limit=12"
+    )
+    jobs = scraper.find_all("div", {"class": "dn-jobdetails__job-card"})
+
 
 publish_or_update(finalJobs)
 
